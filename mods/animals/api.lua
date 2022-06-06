b6a972c7715d1b8b1a3bbd5f28c60126f61ce37c
Izzy Blacklock <izzy.blacklock@gmail.com>
2022-06-05 20:27:52 -0600

    Minor correction of debug print statement

--- mods/animals/api.lua
+++ mods/animals/api.lua
@@ -98,7 +98,7 @@ local energy = mobkit.recall(self,'energy')
 local age = mobkit.recall(self,'age')
 if not age then age=0 end
 if not energy then energy = 0 end
-print ("core_hp_water: "..hp.."ENG: "..energy.."Age: "..age)
+print ("core_hp_water: "..hp.."    ENG: "..energy.."    Age: "..age)
   if hp <= 0 then
     mobkit.clear_queue_high(self)
     animals.handle_drops(self)
