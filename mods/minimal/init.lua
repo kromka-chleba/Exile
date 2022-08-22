51faf9530838d4b2779756b893afa934153d2f8f
Izzy Blacklock <izzy.blacklock@gmail.com>
2022-08-22 11:42:35 -0600

    Testing 16 slot hotbar

--- mods/minimal/init.lua
+++ mods/minimal/init.lua
@@ -45,7 +45,8 @@ minetest.register_on_joinplayer(function(player)
 	player:set_formspec_prepend(formspec)
 
 	-- Set hotbar textures
-	player:hud_set_hotbar_image("gui_hotbar.png")
+	player:hud_set_hotbar_image("gui_hotbar16.png")
+	player:hud_set_hotbar_itemcount(16)
 	player:hud_set_hotbar_selected_image("gui_hotbar_selected.png")
 end)
 
