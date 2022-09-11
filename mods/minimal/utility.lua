6d0f3ce869b6d2c61b826dc5474250fadcb74e86
Izzy Blacklock <izzy.blacklock@gmail.com>
2022-09-11 13:26:05 -0600

    Bug fix - get_int returns 0 on nil values
    combined fire blocks were turning to ash right away because they had
    fuel=0 value

--- mods/minimal/utility.lua
+++ mods/minimal/utility.lua
@@ -58,8 +58,8 @@ function minimal.slabs_combine(pos, node, itemstack, swap_node)
 	if itemstack:get_name() == node.name then
 		-- combine slabs
 		local stack_meta = itemstack:get_meta()
-		local fuel = stack_meta:get_int("fuel")
-		if fuel ~= nil then
+		if meta_table and meta_table.fuel ~= nil then
+			local fuel = meta_table.fuel
 			local pt_meta = minetest.get_meta(pos)
 			fuel = fuel + pt_meta:get_int("fuel")
 			pt_meta:set_int("fuel",fuel)
