2bdc94def08739f440dfed8989099fccc5ccd961
lord_of_the_dumpster <lord_of_the_dumpster@noreply.codeberg.org>
2022-11-02 20:45:32 +0100

    Bugfix: Soils now get wet during rain instead of turing into wet sediments. (#255)
    Co-authored-by: Jan Wielkiewicz <tona_kosmicznego_smiecia@interia.pl>
    Reviewed-on: https://codeberg.org/Mantar/Exile/pulls/255
    Co-authored-by: lord_of_the_dumpster <lord_of_the_dumpster@noreply.codeberg.org>
    Co-committed-by: lord_of_the_dumpster <lord_of_the_dumpster@noreply.codeberg.org>

--- mods/nodes_nature/sediment.lua
+++ mods/nodes_nature/sediment.lua
@@ -252,6 +252,7 @@ function soil.register_dry(soil)
         tiles = {soil.texture_name, sed.texture_name,
                  {name = sed.texture_name.."^"..soil.texture_side_name}},
         _ag_soil = sed.ag_soil,
+        _wet_name = soil.wet_node_name,
     }
     local sed_props = get_dry_node_props(sed)
     local soil_props = merge_tables(sed_props, additional_properties)
@@ -266,6 +267,7 @@ function soil.register_wet(soil)
         tiles = {soil.texture_name.."^"..textures.wet, sed.texture_name.."^"..textures.wet,
                  {name = sed.texture_name.."^"..soil.texture_side_name.."^"..textures.wet}},
         _ag_soil = sed.ag_soil_wet,
+        _dry_name = dry_node_name,
     }
     local sed_props = get_wet_node_props(sed)
     local soil_props = merge_tables(sed_props, additional_properties)
