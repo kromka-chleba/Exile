644f43518b4fff6ca3efc78966909fbab6e92fdc
Jan Wielkiewicz <tona_kosmicznego_smiecia@interia.pl>
2022-11-28 00:29:36 +0100

    Minor compost changes, renaming, porting to new API.

--- mods/nodes_nature/compost.lua
+++ mods/nodes_nature/compost.lua
@@ -5,27 +5,30 @@ local S = nodes_nature.S
 
 -- Compost
 -----------------------------------
-local compost_fermenting_time = 18000 -- 15 in-game days
+local compost_decomposing_time = 18000 -- 15 in-game days
+local decomposition_interval = 600
+local dry_speed = 600
+local wet_speed = 800
 
-local function start_fermenting(pos)
+local function start_decomposing(pos)
     local meta = minetest.get_meta(pos)
-    local ferment = meta:get_int("ferment")
-    if ferment < 1 then
-        meta:set_int("ferment", compost_fermenting_time)
+    local decomposition = meta:get_int("decomposition")
+    if decomposition < 1 then
+        meta:set_int("decomposition", compost_decomposing_time)
     end
-    minetest.get_node_timer(pos):start(600) -- every 600 seconds
+    minetest.get_node_timer(pos):start(decomposition_interval)
 end
 
-local function save_to_inventory(pos, digger, unfermented_name)
+local function save_to_inventory(pos, digger, undecomposed_name)
     if not digger then return false end
     if minetest.is_protected(pos, digger:get_player_name()) then
         return false
     end
     local meta = minetest.get_meta(pos)
-    local ferment = meta:get_int("ferment")
-    local new_stack = ItemStack(unfermented_name)
+    local decomposition = meta:get_int("decomposition")
+    local new_stack = ItemStack(undecomposed_name)
     local stack_meta = new_stack:get_meta()
-    stack_meta:set_int("ferment", ferment)
+    stack_meta:set_int("decomposition", decomposition)
     minetest.remove_node(pos)
     local player_inv = digger:get_inventory()
     if player_inv:room_for_item("main", new_stack) then
@@ -38,29 +41,29 @@ end
 local function restore_from_inventory(pos, itemstack)
     local meta = minetest.get_meta(pos)
     local stack_meta = itemstack:get_meta()
-    local ferment = stack_meta:get_int("ferment")
-    if ferment < 1 then
-        meta:set_int("ferment", compost_fermenting_time)
+    local decomposition = stack_meta:get_int("decomposition")
+    if decomposition < 1 then
+        meta:set_int("decomposition", compost_decomposing_time)
     else
-        meta:set_int("ferment", ferment)
+        meta:set_int("decomposition", decomposition)
     end
 end
 
-local function ferment_compost(pos, fermented_name, speed)
+local function decompose_compost(pos, decomposed_name, speed)
     local meta = minetest.get_meta(pos)
-    local ferment = meta:get_int("ferment")
-    if ferment < 1 then
-        minetest.swap_node(pos, {name = fermented_name})
+    local decomposition = meta:get_int("decomposition")
+    if decomposition < 1 then
+        minetest.swap_node(pos, {name = decomposed_name})
         return false
     else
-        meta:set_int("ferment", ferment - speed)
+        meta:set_int("decomposition", decomposition - speed)
         return true
     end
 end
 
 local compost =
     sediment.new({name = "compost",
-                  description = S("Fermented Compost"),
+                  description = S("Decomposed Compost"),
                   hardness = sediment.hardness.soft,
                   fertility = 1, sound = sediment.sounds.dirt,
                   sound_wet = sediment.sounds.dirt_wet})
@@ -68,32 +71,36 @@ local compost =
 sediment.register_dry(compost)
 sediment.register_wet(compost)
 sediment.register_wet_salty(compost)
+sediment.register_stair_and_slab(compost)
 
-local compost_unfermented =
-    sediment.new({name = "compost_unfermented",
-                  description = S("Unfermented Compost"),
+local compost_undecomposed =
+    sediment.new({name = "compost_undecomposed",
+                  description = S("Undecomposed Compost"),
                   hardness = sediment.hardness.soft,
                   fertility = 1, sound = sediment.sounds.dirt,
                   sound_wet = sediment.sounds.dirt_wet})
 
-sediment.register_dry(compost_unfermented)
-sediment.register_wet(compost_unfermented)
-sediment.register_wet_salty(compost_unfermented)
+sediment.register_dry(compost_undecomposed)
+sediment.register_wet(compost_undecomposed)
+sediment.register_wet_salty(compost_undecomposed)
+sediment.register_stair_and_slab(compost_undecomposed)
 
-local dry_speed = 600
-local wet_speed = 800
+local compost_dry_name = sediment.get_dry_name("compost")
+local compost_wet_name = sediment.get_wet_name("compost")
+local undecomposed_dry_name = sediment.get_dry_name("compost_undecomposed")
+local undecomposed_wet_name = sediment.get_wet_name("compost_undecomposed")
 
 minetest.override_item(
-    sediment.get_dry_name("compost_unfermented"),
+    undecomposed_dry_name,
     {
         on_timer = function(pos, elapsed)
-            return ferment_compost(pos, compost.dry_node_name, dry_speed)
+            return decompose_compost(pos, compost_dry_name, dry_speed)
         end,
         on_construct = function(pos)
-            start_fermenting(pos)
+            start_decomposing(pos)
         end,
         on_dig = function(pos, node, digger)
-            save_to_inventory(pos, digger, compost_unfermented.dry_node_name)
+            save_to_inventory(pos, digger, undecomposed_dry_name)
         end,
         after_place_node = function(pos, placer, itemstack, pointed_thing)
             restore_from_inventory(pos, itemstack)
@@ -101,16 +108,16 @@ minetest.override_item(
 })
 
 minetest.override_item(
-    sediment.get_wet_name("compost_unfermented"),
+    undecomposed_wet_name,
     {
         on_timer = function(pos, elapsed)
-            return ferment_compost(pos, compost.wet_node_name, wet_speed)
+            return decompose_compost(pos, compost_wet_name, wet_speed)
         end,
         on_construct = function(pos, wet_speed)
-            start_fermenting(pos)
+            start_decomposing(pos)
         end,
         on_dig = function(pos, node, digger)
-            save_to_inventory(pos, digger, compost_unfermented.wet_node_name)
+            save_to_inventory(pos, digger, undecomposed_wet_name)
         end,
         after_place_node = function(pos, placer, itemstack, pointed_thing)
             restore_from_inventory(pos, itemstack)
@@ -119,7 +126,7 @@ minetest.override_item(
 
 crafting.register_recipe({
 	type = "shovel_agriculture",
-	output = "nodes_nature:compost_unfermented",
+	output = "nodes_nature:compost_undecomposed",
 	items = {"group:fibrous_plant 16"},
 	level = 1,
 	always_known = true,
