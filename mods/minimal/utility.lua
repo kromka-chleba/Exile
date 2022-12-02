25a5865a940cd8b1b60ed6e87f7ee122da864668
Jeremy <jeremylshannon@gmail.com>
2022-12-01 22:16:24 -0800

    Add ; to the string blacklist too

--- mods/minimal/utility.lua
+++ mods/minimal/utility.lua
@@ -120,7 +120,7 @@ function minimal.click_count_ready(name, pos, count, timeout)
 end
 
 function minimal.sanitize_string(badstring)
-   local disallowed = { "\\", "{", "}", "^",
+   local disallowed = { "\\", "{", "}", "^", ";",
 			--lua magic characters
 			"%(", "%)", "%[", "%]", "%.", "%$",
 			"%^", "%%", "%+", "%-", "%*", "%?"  }
