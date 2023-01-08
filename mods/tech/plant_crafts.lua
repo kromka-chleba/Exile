b9a3ca4ac717b6d02dd635242112485c3dfb68ac
Jan Wielkiewicz <tona_kosmicznego_smiecia@interia.pl>
2023-01-08 23:40:13 +0100

    Bugfix the potatoes again...

--- mods/tech/plant_crafts.lua
+++ mods/tech/plant_crafts.lua
@@ -292,7 +292,7 @@ crafting.register_recipe({
 crafting.register_recipe({
 	type = {"crafting_spot","knife", "mortar_and_pestle"},
 	output = "tech:peeled_anperla",
-	items = {"nodes_nature:anperla_tuber"},
+	items = {"nodes_nature:anperla_root"},
 	level = 1,
 	always_known = true,
 })
@@ -304,14 +304,14 @@ crafting.register_recipe({
 crafting.register_recipe({
 	type = "mortar_and_pestle",
 	output = "tech:peeled_anperla 6",
-	items = {"nodes_nature:anperla_tuber 6"},
+	items = {"nodes_nature:anperla_root 6"},
 	level = 1,
 	always_known = true,
 })
 crafting.register_recipe({
 	type = "mortar_and_pestle",
 	output = "tech:peeled_anperla 36",
-	items = {"nodes_nature:anperla_tuber 36"},
+	items = {"nodes_nature:anperla_root 36"},
 	level = 1,
 	always_known = true,
 })
