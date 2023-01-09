3a504e1f1642ae3d9589d2dd0fb24fa3b74c0153
Jeremy <jeremylshannon@gmail.com>
2023-01-08 20:40:42 -0800

    Fix "days survived" to work properly in multiplayer

--- mods/lore/char_tab.lua
+++ mods/lore/char_tab.lua
@@ -7,12 +7,27 @@ Player stats etc
 
 ]]
 
+lore = lore
+
 ------------------------------------
---set character name and record start date
+--set character name and record start time
+
+local function update_playtime(player, meta)
+   if not meta then
+      meta = player:get_meta()
+   end
+   local last = tonumber(meta:get_int("char_time_stamp"))
+   local difference = minetest.get_gametime() - last
+   local time = tonumber(meta:get_int("char_time_survived"))
+   meta:set_int("char_time_survived", time + difference)
+   meta:set_int("char_time_stamp", minetest.get_gametime())
+end
+
 minetest.register_on_newplayer(function(player)
   local meta = player:get_meta()
   meta:set_string("char_name", lore.generate_name(3))
-  meta:set_int("char_start_date", minetest.get_day_count())
+  meta:set_int("char_time_stamp", minetest.get_gametime())
+  meta:set_int("char_time_survived", 0)
   meta:set_string("bio", lore.generate_bio(player))
   meta:set_int("lives", 1)
 end)
@@ -20,6 +35,18 @@ end)
 minetest.register_on_joinplayer(function(player)
   local meta = player:get_meta()
   local lives = ( meta:get_int("lives") or 1 )
+  local old = tonumber(meta:get_string("char_start_date"))
+  if old then -- Character used the old busted "days lived" setup, migrate
+     if minetest.is_singleplayer() then
+	-- It was really only valid for singleplayer
+	local dayslived = minetest.get_day_count() - old
+	meta:set_int("char_time_survived", dayslived * 1200) -- seconds
+     else -- Just clear it and start over for multiplayer
+	meta:set_int("char_time_survived", 0)
+     end
+     meta:set_string("char_start_date", "") -- Migration over
+  end
+  meta:set_int("char_time_stamp", minetest.get_gametime())
   if lives == 0 then lives = 1 end
   meta:set_int("lives", lives)
 end)
@@ -27,11 +54,34 @@ end)
 minetest.register_on_respawnplayer(function(player)
   local meta = player:get_meta()
   meta:set_string("char_name", lore.generate_name(3))
-  meta:set_int("char_start_date", minetest.get_day_count())
+  meta:set_int("char_time_stamp", minetest.get_gametime())
+  meta:set_int("char_time_survived", 0)
   meta:set_string("bio", lore.generate_bio(player))
   local lives = meta:get_int("lives") or 1
   meta:set_int("lives", lives + 1)
 end)
+
+minetest.register_on_leaveplayer(function(player)
+  local meta = player:get_meta()
+  local last = tonumber(meta:get_int("char_time_stamp"))
+  local difference = minetest.get_gametime() - last
+  local time = tonumber(meta:get_int("char_time_survived"))
+  meta:set_int("char_time_survived", time + difference)
+  meta:set_int("char_time_stamp", minetest.get_gametime())
+end)
+
+local time = 0
+minetest.register_globalstep(function(dtime)
+      time = time + dtime
+      if time > 60 then
+	 -- update all players
+	 for _, player in pairs(minetest.get_connected_players()) do
+	    update_playtime(player)
+	 end
+	 time = 0
+      end
+end)
+
 ------------------------------------
 
 --Forms for sfinv
@@ -39,12 +89,14 @@ end)
 
 --get data and create form
 local function sfinv_get(self, player, context)
-	local meta = player:get_meta()
-	local name = meta:get_string("char_name")
-  local days = minetest.get_day_count() - meta:get_int("char_start_date")
+  local meta = player:get_meta()
+  local name = meta:get_string("char_name")
+  update_playtime(player, meta)
+  local tsurv = tonumber(meta:get_string("char_time_survived"))
+  local days = math.floor( tsurv / 1200 )
   local lives = meta:get_int("lives")
-  local effects_list = meta:get_string("effects_list")
-  local effects_list = minetest.deserialize(effects_list) or {}
+  local effects_list_str = meta:get_string("effects_list")
+  local effects_list = minetest.deserialize(effects_list_str) or {}
   local bio = meta:get_string("bio")
   --backwards compatibility
   if bio == "" then
