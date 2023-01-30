183c44e61bd0821866ccd93174424da9cb0e1fa6
noodles <noodles358@gmail.com>
2023-01-30 02:17:39 -0500

    tree trunks now grow through ancient rings

--- mods/minimal/debug.lua
+++ mods/minimal/debug.lua
@@ -3,7 +3,7 @@
 --
 exile = exile
 exile.debug = {}
-__DEBUG__ = false
+__DEBUG__ = true
 
 function exile.debug.print(message)
 	if __DEBUG__ then
@@ -20,13 +20,27 @@ function exile.debug.crafting_stations(station)
 	end
 end
 
-
-
-
+--[[
 if __DEBUG__ then
 	minetest.register_on_mods_loaded(function()
 		print("--------------------[ Modules Loaded [-----------------------------")
 		exile.debug.crafting_stations('axe_mixing')
 	end)
 end
+--]]
 
+function exile.debug.log_to_world(message, filename)
+	if __DEBUG__ then
+		local wpath = minetest.get_worldpath()
+		local wname = wpath:match( "([^/\\]+)$" )
+		filename = filename or (wname..'_debug.log')  -- default val
+		local filespec = wpath..'/'..filename
+		local file, err = io.open( filespec, 'a')
+		if (err ~= nil) then
+	   		return
+		end
+		file:write( message )
+		file:flush()
+		file:close()
+	end
+end
