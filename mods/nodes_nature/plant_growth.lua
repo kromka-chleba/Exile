c83cdc129a2afd0d06f5b504a372ce6f45742a59
Jan Wielkiewicz <tona_kosmicznego_smiecia@interia.pl>
2023-02-02 03:48:56 +0100

    Plant API: Fix crash on placing seeds of a plant that has no place_param2 (nodebox plants).

--- mods/nodes_nature/plant_growth.lua
+++ mods/nodes_nature/plant_growth.lua
@@ -379,8 +379,10 @@ local function add_to_param2(pos, nr)
         local seedling_name = nodedef._next_life_stage
         nodedef = minetest.registered_nodes[seedling_name]
     end
-    local new_param2 = nodedef.place_param2 + nr
-    minetest.set_node(pos, {name = name, param2 = new_param2})
+    if nodedef.place_param2 then
+        local new_param2 = nodedef.place_param2 + nr
+        minetest.set_node(pos, {name = name, param2 = new_param2})
+    end
 end
 
 function plant.set_to_domesticated(pos)
