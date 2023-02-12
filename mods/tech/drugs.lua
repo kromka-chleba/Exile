07c115c405fb318779b2d3b069b3d82764ab81a2
Jan Wielkiewicz <tona_kosmicznego_smiecia@interia.pl>
2023-02-12 02:14:05 +0100

    Tech: Drugs: Use flowering hakimi for herbal medicine.

--- mods/tech/drugs.lua
+++ mods/tech/drugs.lua
@@ -297,7 +297,7 @@ minetest.register_node("tech:tang_unfermented", {
 crafting.register_recipe({
 	type = "mortar_and_pestle",
 	output = "tech:herbal_medicine",
-	items = {'nodes_nature:hakimi', 'nodes_nature:merki', 'nodes_nature:moss'},
+	items = {'nodes_nature:hakimi_flowering', 'nodes_nature:merki', 'nodes_nature:moss'},
 	level = 1,
 	always_known = true,
 })
