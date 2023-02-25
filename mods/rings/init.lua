3ea11be8f82affa1a31423d77fd2b2aa2e123ca4
noodles <noodles@noreply.codeberg.org>
2023-02-25 23:35:42 +0000

    tree trunks now grow through ancient rings (#262)
    ready for merge. to test, try with valleys seed 14116200462208576059 and look at the ring near (250,50,-200)
    
    Co-authored-by: noodles <noodles358@gmail.com>
    Reviewed-on: https://codeberg.org/Mantar/Exile/pulls/262
    Co-authored-by: noodles <noodles@noreply.codeberg.org>
    Co-committed-by: noodles <noodles@noreply.codeberg.org>

--- mods/rings/init.lua
+++ mods/rings/init.lua
@@ -48,8 +48,37 @@ minetest.register_node("rings:moon_glass", {
 })
 
 
-local c_ring = minetest.get_content_id("rings:antiquorium")
-local c_ring_i = minetest.get_content_id("rings:moon_glass") -- TODO: replace this with a glowing material
+local c_ring_o = minetest.get_content_id("rings:antiquorium")	-- outer shell material
+local c_ring_i = minetest.get_content_id("rings:moon_glass") 	-- inner shell material -- TODO: replace this with a glowing material
+
+
+--[[ allow tree trunks to "grow" through rings by not deleting them during ring creation --]]
+-- c_node_list is an integer-keyed table for fast lookups during the x-y-z loop
+-- register after mods are loaded so all tree node content ids are available
+
+local c_node_list = {}
+minetest.register_on_mods_loaded(function()
+	-- get content ids for all tree types
+	local tree_list = {}
+	tree_list.maraka  = minetest.get_content_id("nodes_nature:maraka_tree" )
+	tree_list.tangkal = minetest.get_content_id("nodes_nature:tangkal_tree")
+	tree_list.sasaran = minetest.get_content_id("nodes_nature:sasaran_tree")
+	tree_list.kagum   = minetest.get_content_id("nodes_nature:kagum_tree" )
+	tree_list.panasee = minetest.get_content_id("nodes_nature:panasee_tree" )
+	tree_list.amma    = minetest.get_content_id("nodes_nature:amma_tree" )
+	tree_list.daoja   = minetest.get_content_id("nodes_nature:daoja_tree" )
+	
+	for _ , v in pairs(tree_list) do
+		c_node_list[v] = true
+	end
+
+	-- debug info.  uncomment here and set __DEBUG__ = true in minimal/debug.lua to enable
+	--exile.debug.log_to_world( "c_ring_o = " .. c_ring_o .. "\n" )
+	--exile.debug.log_to_world( "c_ring_i = " .. c_ring_i .. "\n" )
+	--exile.debug.log_to_world( "tree_list = " .. dump(tree_list) .. "\n" )
+	--exile.debug.log_to_world( "c_node_list = " .. dump(c_node_list) .. "\n\n" )
+end)
+
 
 --noise parameters
 -- base terrain height
@@ -82,56 +111,83 @@ local nobj_3d_r = noise_handler.get_noise_object(np_3d_ring, chunk_size)
 
 local data = {}
 
+
 minetest.register_on_generated(function(minp, maxp, blockseed)
-      if maxp.y < 40 or minp.y > 80 then  -- not too high or low
-	 return
-      end
-      if math.floor(blockseed / 100) % 70 > 0 then return end
-      local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
-      local area = VoxelArea:new({MinEdge=emin, MaxEdge=emax})
-      vm:get_data(data)
-      local nvals_2d_b = nobj_2d_b:get_2d_map_flat(minp)
-      local nvals_3d_r
+    -- check block elevation (not too high or low)
+    if maxp.y < 40 or minp.y > 80 then 
+		return
+	end
 
-      math.randomseed(blockseed)
+	-- 1-in-70 chance of generating a ring
+    if math.floor(blockseed / 100) % 70 > 0 then
+    	return
+    end
 
-      local rdir, rpos
-      nvals_3d_r = nobj_3d_r:get_3d_map_flat(minp)
-      rdir = vector.new(math.random() - 0.5, (math.random() - 0.5)/2, math.random() - 0.5)
+    -- get voxelmanip node data
+    local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
+    local area = VoxelArea:new({MinEdge=emin, MaxEdge=emax})
+    vm:get_data(data)
+    
+    -- noise & randomness 
+    local nvals_2d_b = nobj_2d_b:get_2d_map_flat(minp)
+    local nvals_3d_r = nobj_3d_r:get_3d_map_flat(minp)
+    
+    math.randomseed(blockseed)
 
-      rpos = {}
-      rpos.x = math.floor((minp.x + maxp.x) / 2)
-      rpos.z = math.floor((minp.z + maxp.z) / 2)
-      rpos.y = math.floor(nvals_2d_b[(rpos.z-minp.z) * 80 + (rpos.x-minp.x) + 1])
+    -- ring axis direction and centroid position
+    -- axis vector is random inside a 1.0 x 0.5 x 1.0 box, centered on (0,0,0)
+    -- centroid is horizontally centered in the chunk, with elevation based on 2d noise
+    local rdir, rpos
+    rdir = vector.new(math.random() - 0.5, (math.random() - 0.5)/2, math.random() - 0.5)
+    rpos = {}
+    rpos.x = math.floor((minp.x + maxp.x) / 2)
+    rpos.z = math.floor((minp.z + maxp.z) / 2)
+    rpos.y = math.floor(nvals_2d_b[(rpos.z-minp.z) * 80 + (rpos.x-minp.x) + 1])
 
-      for z = minp.z, maxp.z do
-	 for y = minp.y, maxp.y do
+    -- debug info.  uncomment here and set __DEBUG__ = true in minimal/debug.lua to enable
+	--exile.debug.log_to_world( "\nblockseed = " .. blockseed .. "\n" )
+	--exile.debug.log_to_world( "emin=" .. dump(emin) .. ")\n" )
+	--exile.debug.log_to_world( "emax=" .. dump(emax) .. ")\n" )
+    --exile.debug.log_to_world( "rpos=(" .. rpos.x .. ',' .. rpos.y .. ',' .. rpos.z .. ")\n" )
+	--exile.debug.log_to_world( "rdir=(" .. rdir.x .. ',' .. rdir.y .. ',' .. rdir.z .. ")\n" )
+	--exile.debug.log_to_world( "rdir_length=" .. vector.length(rdir) .. "\n" )
+
+	-- shells are defined by node distance from the ring centroid
+	-- use distance squared to avoid computationally expensive square root operations 
+	local dd_o_min = math.pow(30, 2)	-- outer shell min distance squared
+	local dd_i_min = math.pow(31, 2)	-- inner shell min distance squared
+	local dd_i_max = math.pow(34, 2)	-- inner shell max distance squared
+	local dd_o_max = math.pow(35, 2)	-- outer shell max distance squared
+
+    for z = minp.z, maxp.z do
+	  for y = minp.y, maxp.y do
 	    for x = minp.x, maxp.x do
-	       local vi = area:index(x, y, z)
 
-	       if rdir then
-		  local dist = math.pow(rpos.x - x, 2) + math.pow(rpos.z - z, 2) + math.pow(rpos.y - y, 2)
-		  if dist > math.pow(31, 2) and dist < math.pow(34, 2) and
-		     math.abs((x-rpos.x) * rdir.x + (y-rpos.y) * rdir.y + (z-rpos.z) * rdir.z) <= 2 then
-		     data[vi] = c_ring_i
-		  end
-		  local nv_3d_r = nvals_3d_r[(z - minp.z) * 80 * 80 + (y - minp.y) * 80 + (x - minp.x) + 1]
+			local vi = area:index(x, y, z)
 
-		  if data[vi] ~= c_ring_i and nv_3d_r < 0.3 and
-		     dist > math.pow(30, 2) and dist < math.pow(35, 2) and
-		     math.abs((x-rpos.x) * rdir.x + (y-rpos.y) * rdir.y + (z-rpos.z) * rdir.z) <= 3 then
-		     data[vi] = c_ring
-		  end
-	       end
+       		if not c_node_list[data[vi]] then	 -- not at a tree node
+
+		  		local dd_node = math.pow(rpos.x - x, 2) + math.pow(rpos.z - z, 2) + math.pow(rpos.y - y, 2)	-- node distance squared
+		  		local s_proj = math.abs((x-rpos.x) * rdir.x + (y-rpos.y) * rdir.y + (z-rpos.z) * rdir.z)	-- node dist projected on axis and scaled by axis length
+		  		local nv_3d_r = nvals_3d_r[(z - minp.z) * 80 * 80 + (y - minp.y) * 80 + (x - minp.x) + 1]	-- outer shell crack noise
+
+		  		if (dd_node > dd_i_min) and (dd_node < dd_i_max) and (s_proj <= 2.0) then
+		     		data[vi] = c_ring_i  -- place inner shell
+		     	end
+
+		     	if (dd_node > dd_o_min) and (dd_node < dd_o_max) and (s_proj <= 3.0) and (nv_3d_r < 0.3) and (data[vi] ~= c_ring_i) then
+		     		data[vi] = c_ring_o  -- place outer shell
+		  		end
+
+	       	end
 
 	    end
-	 end
-      end
+	  end
+    end
 
-      vm:set_data(data)
-      vm:calc_lighting()
-      vm:write_to_map()
-      if rdir then
-	 minetest.fix_light(minp, maxp)
-      end
+    vm:set_data(data)
+    vm:calc_lighting()
+    vm:write_to_map()
+	minetest.fix_light(minp, maxp)
+
 end)
