7f4ed90c372256e5e64ad0a327d1b69b6d229fce
Jeremy <jeremylshannon@gmail.com>
2023-04-26 18:34:26 -0700

    Move stone spear to hand crafting for now

--- mods/spears/tools.lua
+++ mods/spears/tools.lua
@@ -110,6 +110,6 @@ if minetest.get_modpath("default") then
 elseif minetest.get_modpath("minimal") then
 --TODO Make spears_register_spear() allow registering multiple crafting stations
    spears_register_spear('stone', 'Stone', 8, 20, 'tech:stone_chopper',
-			 "crafting_spot")
+			 "hand")
    spears_register_spear('steel', 'Iron', 14, 30, 'tech:iron_ingot', "anvil")
 end
