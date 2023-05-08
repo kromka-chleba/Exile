a8957ea1c5ffd1a5f40a7f318693b42f23b3ef52
Jeremy <jeremylshannon@gmail.com>
2023-05-07 22:32:37 -0700

    Add an error log for dye bundles that can't get a plant name

--- mods/ncrafting/dyes.lua
+++ mods/ncrafting/dyes.lua
@@ -214,6 +214,8 @@ local function bundlename(meta, plant, treatment)
    end
    if minetest.registered_nodes[plant] then
       fmt_plant = "of "..minetest.registered_nodes[plant].description
+   else
+      minetest.log("error","NCRAFTING: Bundle could not get name of plant: ",plant)
    end
    if treatment and treatment ~= "" then
       fmt_treatment = ", "..methodstring[treatment]
