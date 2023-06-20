5a9c82039b310fed68f4f5cf3c7807bff36da101
Jan Wielkiewicz <tona_kosmicznego_smiecia@interia.pl>
2023-06-20 22:17:26 +0200

    Mapgen: Deco: Add new files: trees.lua for tree decorations, soil.lua for lists of soils.
    init.lua: Add a global deco variable; Add deco.find_schematic that finds schematics in the directory.

---
+++ mods/mapgen/soils.lua
@@ -0,0 +1,132 @@
+-- Soils for decorations
+
+local forest_on = {
+  "nodes_nature:forest_soil", "nodes_nature:forest_soil_wet",
+  "nodes_nature:upland_forest_soil", "nodes_nature:upland_forest_soil_wet",
+}
+
+local woodland_on = {
+  "nodes_nature:woodland_soil", "nodes_nature:woodland_soil_wet",
+  "nodes_nature:upland_woodland_soil", "nodes_nature:upland_woodland_soil_wet",
+}
+
+local rich_forest_on = {
+  "nodes_nature:rich_forest_soil", "nodes_nature:rich_forest_soil_wet",
+  "nodes_nature:rich_woodland_soil", "nodes_nature:rich_woodland_soil_wet",
+}
+
+local wetland_on = {
+  "nodes_nature:swamp_forest_soil", "nodes_nature:swamp_forest_soil_wet",
+  "nodes_nature:marshland_soil", "nodes_nature:marshland_soil_wet",
+}
+
+local swamp_forest_on = {
+  "nodes_nature:swamp_forest_soil", "nodes_nature:swamp_forest_soil_wet",
+}
+
+local marshland_on = {
+  "nodes_nature:marshland_soil", "nodes_nature:marshland_soil_wet",
+}
+
+local grassland_on = {
+  "nodes_nature:coastal_grassland_soil", "nodes_nature:coastal_grassland_soil_wet",
+  "nodes_nature:grassland_soil", "nodes_nature:grassland_soil_wet"
+}
+
+local shrubland_on = {
+  "nodes_nature:coastal_shrubland_soil", "nodes_nature:coastal_shrubland_soil_wet",
+  "nodes_nature:shrubland_soil", "nodes_nature:shrubland_soil_wet"
+}
+
+local grass_shrub_on = {
+  "nodes_nature:coastal_grassland_soil", "nodes_nature:coastal_grassland_soil_wet",
+  "nodes_nature:grassland_soil", "nodes_nature:grassland_soil_wet",
+  "nodes_nature:coastal_shrubland_soil", "nodes_nature:coastal_shrubland_soil_wet",
+  "nodes_nature:shrubland_soil", "nodes_nature:shrubland_soil_wet"
+}
+
+local duneland_on = {
+  "nodes_nature:duneland_soil", "nodes_nature:duneland_soil_wet"
+}
+
+local barrenland_on = {
+  "nodes_nature:barrenland_soil", "nodes_nature:barrenland_soil_wet"
+}
+
+local highland_on = {
+  "nodes_nature:highland_soil", "nodes_nature:highland_soil_wet"
+}
+
+local badland_on = {
+  "nodes_nature:duneland_soil", "nodes_nature:duneland_soil_wet",
+  "nodes_nature:barrenland_soil", "nodes_nature:barrenland_soil_wet",
+  "nodes_nature:highland_soil", "nodes_nature:highland_soil_wet"
+}
+
+local all_soils_on = {
+  "nodes_nature:forest_soil", "nodes_nature:forest_soil_wet",
+  "nodes_nature:upland_forest_soil", "nodes_nature:upland_forest_soil_wet",
+  "nodes_nature:woodland_soil", "nodes_nature:woodland_soil_wet",
+  "nodes_nature:upland_woodland_soil", "nodes_nature:upland_woodland_soil_wet",
+  "nodes_nature:rich_forest_soil", "nodes_nature:rich_forest_soil_wet",
+  "nodes_nature:rich_woodland_soil", "nodes_nature:rich_woodland_soil_wet",
+  "nodes_nature:swamp_forest_soil", "nodes_nature:swamp_forest_soil_wet",
+  "nodes_nature:marshland_soil", "nodes_nature:marshland_soil_wet",
+  "nodes_nature:coastal_grassland_soil", "nodes_nature:coastal_grassland_soil_wet",
+  "nodes_nature:grassland_soil", "nodes_nature:grassland_soil_wet",
+  "nodes_nature:coastal_shrubland_soil", "nodes_nature:coastal_shrubland_soil_wet",
+  "nodes_nature:shrubland_soil", "nodes_nature:shrubland_soil_wet",
+  "nodes_nature:duneland_soil", "nodes_nature:duneland_soil_wet",
+  "nodes_nature:barrenland_soil", "nodes_nature:barrenland_soil_wet",
+  "nodes_nature:highland_soil", "nodes_nature:highland_soil_wet",
+}
+
+local not_badland_soils_on = {
+  "nodes_nature:forest_soil", "nodes_nature:forest_soil_wet",
+  "nodes_nature:upland_forest_soil", "nodes_nature:upland_forest_soil_wet",
+  "nodes_nature:woodland_soil", "nodes_nature:woodland_soil_wet",
+  "nodes_nature:upland_woodland_soil", "nodes_nature:upland_woodland_soil_wet",
+  "nodes_nature:rich_forest_soil", "nodes_nature:rich_forest_soil_wet",
+  "nodes_nature:rich_woodland_soil", "nodes_nature:rich_woodland_soil_wet",
+  "nodes_nature:swamp_forest_soil", "nodes_nature:swamp_forest_soil_wet",
+  "nodes_nature:marshland_soil", "nodes_nature:marshland_soil_wet",
+  "nodes_nature:coastal_grassland_soil", "nodes_nature:coastal_grassland_soil_wet",
+  "nodes_nature:grassland_soil", "nodes_nature:grassland_soil_wet",
+  "nodes_nature:coastal_shrubland_soil", "nodes_nature:coastal_shrubland_soil_wet",
+  "nodes_nature:shrubland_soil", "nodes_nature:shrubland_soil_wet",
+}
+
+local glow_worm_on = {
+  "nodes_nature:granite", "nodes_nature:gneiss", "nodes_nature:limestone", "nodes_nature:jade",
+}
+
+local gravel_on = {
+  "nodes_nature:granite", "nodes_nature:limestone", "nodes_nature:gneiss", "nodes_nature:conglomerate",
+}
+
+local sand_on = {
+  "nodes_nature:granite", "nodes_nature:limestone", "nodes_nature:gneiss", "nodes_nature:sandstone",
+}
+
+local silt_on = {
+  "nodes_nature:granite", "nodes_nature:limestone", "nodes_nature:gneiss", "nodes_nature:siltstone",
+}
+
+local clay_on = {
+  "nodes_nature:granite", "nodes_nature:limestone", "nodes_nature:gneiss", "nodes_nature:claystone",
+}
+
+local cave_mushrooms_on  = {
+  "nodes_nature:silt", "nodes_nature:clay", "nodes_nature:sand", "nodes_nature:gravel",
+  "nodes_nature:silt_wet", "nodes_nature:clay_wet", "nodes_nature:sand_wet", "nodes_nature:gravel_wet",
+}
+
+local fish_on = {
+  "nodes_nature:silt_wet_salty", "nodes_nature:sand_wet_salty","nodes_nature:gravel_wet_salty",
+}
+
+local cave_egg_on = {
+  "nodes_nature:granite", "nodes_nature:limestone", "nodes_nature:ironstone", "nodes_nature:gneiss",
+  "nodes_nature:granite_boulder", "nodes_nature:limestone_boulder", "nodes_nature:ironstone_boulder", "nodes_nature:gneiss_boulder",
+  "nodes_nature:sandstone", "nodes_nature:siltstone", "nodes_nature:claystone", "nodes_nature:conglomerate",
+}
