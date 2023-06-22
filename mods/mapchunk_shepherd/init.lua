143aa3c0388ff2da490eff39b572f4f9c05b4d36
Jan Wielkiewicz <tona_kosmicznego_smiecia@interia.pl>
2023-06-23 01:03:26 +0200

    Mapchunk Shepherd: Initial code.
    This mod will be responsible for tracking mapchunks visited by players.
    Mapchunk Shepherd will allow running Voxel Manip on mapchunks with specific content.

---
+++ mods/mapchunk_shepherd/init.lua
@@ -0,0 +1,7 @@
+---------------------------------------------
+---- Mapchunk shepherd
+---------------------------------------------
+
+local modpath=minetest.get_modpath('mapchunk_shepherd')
+
+dofile(modpath.."/shepherd.lua")
