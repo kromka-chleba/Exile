aeb2dd16c4bf57063ee5d2837407e4e01b5e645e
Jan Wielkiewicz <tona_kosmicznego_smiecia@interia.pl>
2023-06-24 01:14:27 +0200

    Mapchunk shepherd: Add code for registering, encoding and decoding binary labels.

--- mods/mapchunk_shepherd/init.lua
+++ mods/mapchunk_shepherd/init.lua
@@ -2,6 +2,10 @@
 ---- Mapchunk shepherd
 ---------------------------------------------
 
-local modpath=minetest.get_modpath('mapchunk_shepherd')
+-- Globals
+mapchunk_shepherd = {}
 
+local modpath = minetest.get_modpath('mapchunk_shepherd')
+
+dofile(modpath.."/labels.lua")
 dofile(modpath.."/shepherd.lua")
