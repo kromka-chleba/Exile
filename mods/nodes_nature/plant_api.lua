0bbcf8c049da0664f2439338c9587085af0504d4
Jan Wielkiewicz <tona_kosmicznego_smiecia@interia.pl>
2023-07-10 19:47:49 +0200

    Plant API: Fix a race condition that made wild fruiting plants die after punching.

--- mods/nodes_nature/plant_api.lua
+++ mods/nodes_nature/plant_api.lua
@@ -589,11 +589,11 @@ end
 local function fruiting_on_punch(pos, node, puncher, pointed_thing)
     local node_name = minetest.get_node(pos).name
     local nodedef = minetest.registered_nodes[node_name]
-    if nodedef._fruitless_name then
-        minimal.force_place_keep_param2(pos, nodedef._fruitless_name)
-    end
     if node.param2 < 64 then
         plant.set_to_half_wild(pos)
+    end
+    if nodedef._fruitless_name then
+        minimal.force_place_keep_param2(pos, nodedef._fruitless_name)
     end
     local inv = puncher:get_inventory()
     local new_stack = ItemStack(nodedef._fruit_name)
