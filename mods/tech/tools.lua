b5908b8566c41ff48b7e69abf8b28ebc3f3c4cc6
TPH <DamotrixRob@gmail.com>
2023-07-15 11:22:38 -0400

    Tools being destroyed by "bamboo" plants fixed #330

--- mods/tech/tools.lua
+++ mods/tech/tools.lua
@@ -39,6 +39,11 @@ local function place_tool(itemstack, placer, pointed_thing, placed_name)
         minetest.registered_nodes[under_front.name].walkable then
         -- check if the pointed item has on_rightclick ...
         if not minetest.registered_nodes[under.name].on_rightclick then
+            -- if they're both numbers somehow, best to assume it's a bamboo plant lol
+            if (type(minetest.registered_nodes[above.name].groups.woody_plant) == "number" and type(minetest.registered_nodes[above.name].groups.cane_plant) == "number") then 
+              -- replace bamboo with air so that the tool places appropriately
+              minetest.swap_node(pointed_thing.above,minetest.registered_nodes["air"])
+            end
             local wear = itemstack:get_wear()
             -- place if not
             itemstack:take_item(1)
