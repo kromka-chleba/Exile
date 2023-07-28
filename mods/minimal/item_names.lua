bfe1e80aaeead1ccfe87f2c89d18e5f65030f373
Dan McNeill <danmcne@gmail.com>
2023-07-28 13:49:45 +0200

    Added sun hat and sandals

--- mods/minimal/item_names.lua
+++ mods/minimal/item_names.lua
@@ -62,7 +62,7 @@ minetest.register_globalstep(function(dtime)
 			data.dtime = 0
 
 			local desc = stack.get_meta
-				and stack:get_meta():get_string("description")
+				and stack:get_short_description()
 
 			if not desc or desc == "" then
 				-- Try to use default description when none is set in the meta
