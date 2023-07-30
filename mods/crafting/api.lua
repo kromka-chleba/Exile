3b679a92fc4c453aa9c53c7fee4feb51426e3307
TPH <DamotrixRob@gmail.com>
2023-07-30 18:40:01 -0400

    ItemStack max is now respected in crafting. Fixes #184

--- mods/crafting/api.lua
+++ mods/crafting/api.lua
@@ -324,22 +324,22 @@ function crafting.perform_craft(name, inv, listname, outlistname, recipe)
   local subtract_loop = math.ceil(count / max_amt) -- crafted stack size divided by its stack max then ceil'd (so a stack max and a half will not be 1.5 but rather 2)
 	-- Loop time
   for _ = 1, subtract_loop, 1 do -- loop through the amount of times stack is over its max, if 1 then only run code once
-    if (subtract_loop > 1) then -- if the count is greater than stack max otherwise don't try to split up itemstack
-      if (count > max_amt) then -- if count is still greater than stack max
-        itemstack:set_count(max_amt) -- set stack to max
-        count = count - max_amt -- lower count by stack max
-      elseif (count > 0) then -- else and if count is not below or is 0
-        itemstack:set_count(count) -- set itemstack to count
-      end
+    if (count > max_amt) then
+      itemstack:set_count(max_amt) -- set stack to max
+      count = count - max_amt -- lower count by stack max
+    elseif (count > 0) then -- if no modifications necessary then just set it to count
+      itemstack:set_count(count)
     end
     
     -- add output
-    if inv:room_for_item("main", itemstack) then
-       inv:add_item(outlistname, itemstack)
-    else
-       local pos = minetest.get_player_by_name(name):get_pos()
-       minetest.chat_send_player(name, "No room in inventory!")
-       minetest.add_item(pos, itemstack)
+    if (count > 0) then -- just in case something goes wrong and an itemstack below or equal to 0 in count is made
+      if inv:room_for_item("main", itemstack) then
+         inv:add_item(outlistname, itemstack)
+      else
+         local pos = minetest.get_player_by_name(name):get_pos()
+         minetest.chat_send_player(name, "No room in inventory!")
+         minetest.add_item(pos, itemstack)
+      end
     end
   end
   
