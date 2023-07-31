5ffed82c55cbfa607f8c1228484c47dc361a4b7b
TPH <tph@noreply.codeberg.org>
2023-07-31 22:20:06 +0000

    Fixes #358 properly (animals not dying to high temps)  and fixes #313 (animal drops) (#389)
    ihapless' https://codeberg.org/Mantar/Exile/pulls/377 did not correctly kill animals upon high temps (or in time anyways). To assist with instantanious death, I set the energy to 0, exhausting the animal and forcing death. Line 174
    
    Added "```get_temp_mean(pos)```" to get all of the temperatures around a problematic high temp node and mathematically "mean" it to verify if the animal should die or not. Creates several positions via a matrix and runs ```get_point_temp(pos)``` on each to get temps all around, then divides it by the total of the pointed nodes (should be 27). Code only runs "```get_temp_mean(pos)```" only if the first ```get_point_temp(pos)``` returns a temperature higher than 100C. No more killing animals with 1 torch. Code could be moved to climate or minimal, however currently is defined as a "local function" inside of animals.lua .
    Lines 114 - 139
    
    I fix issue #313 with the animal drops not correlating to the final resting place of the dead animal. I simply get the code to check for the animal's body's position after the 5 second minetest.after(), but ensure two checks to prevent crash. First check to check if self.object (entity) is "userdata" (existing), and second an "or" statement between either getting a new position or using the old defined position (not doing this caused some crashes here and there, I assume due to the entity not being able to get a pos).
    Lines 61-63
    
    Co-authored-by: TPH <DamotrixRob@gmail.com>
    Reviewed-on: https://codeberg.org/Mantar/Exile/pulls/389
    Co-authored-by: TPH <tph@noreply.codeberg.org>
    Co-committed-by: TPH <tph@noreply.codeberg.org>

--- mods/animals/api.lua
+++ mods/animals/api.lua
@@ -58,6 +58,9 @@ function animals.handle_drops(self)
      if chance < (100/item.chance) then
        --leave time for death animation to end
        minetest.after(5, function()
+         if (type(self.object) == "userdata") then -- if entity then
+           pos = self.object:get_pos() or pos -- get entity's pos or if pos is nil, use old pos
+         end
          minetest.add_item(pos, item.name.." "..tostring(amount))
        end)
      end
@@ -107,6 +110,34 @@ if not energy then energy = 0 end
   end
 end
 
+
+local function get_mean_temp(pos) -- this could be put somewhere else like in climate or minimal
+  local temps = {}
+  
+  if (type(pos) ~= "table") then -- no pos table is given then
+    return 15
+  elseif (type(pos.x) ~= "number" or type(pos.y) ~= "number" or type(pos.z) ~= "number") then -- incase an invalid pos is given
+    return 15
+  end
+  
+  for x = -1, 1, 1 do -- create matrix of possible positions
+    for y = -1, 1, 1 do
+      for z = -1, 1, 1 do
+        local npos = {x = (pos.x - x), y = (pos.y - y), z = (pos.z - z)} -- matrix the pos :D
+        
+        temps[#temps + 1] = climate.get_point_temp(npos)
+      end
+    end
+  end
+  
+  local mtemp = 0 -- start with a number so it can be calculated
+  for _,num in pairs(temps) do
+    mtemp = mtemp + num
+  end
+  
+  return mtemp / #temps -- return the "mean" of the matrix'd temps
+end
+
 ----------------------------------------------------
 --core health, energy and age
 function animals.core_life(self, lifespan, pos)
@@ -135,8 +166,12 @@ function animals.core_life(self, lifespan, pos)
 
   --die from high temp
   local temp = climate.get_point_temp(pos)
-  if temp > 100 then
-     energy = energy - temp
+  if (temp > 100) then -- get the mathematical "mean" of the pos and the surroundings nodes
+    temp = get_mean_temp(pos)
+  end
+  if temp > 100 then -- if it's still boiling time
+    -- make the animal exhausted and hurt them (instant death)
+     energy = 0
      mobkit.hurt(self, temp)
   end
 
