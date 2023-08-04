a8c38b42654507b0c8db9e94ca873af8914832b4
Jan Wielkiewicz <tona_kosmicznego_smiecia@interia.pl>
2023-08-04 20:14:58 +0200

    Mapchunk Shepherd: Fix disappearing cana bug in the top placer.
    The bug was caused by the snow placer.
    Nodes were overwritten by nil IDs.

--- mods/mapchunk_shepherd/dogs.lua
+++ mods/mapchunk_shepherd/dogs.lua
@@ -18,17 +18,13 @@ local air_id = minetest.get_content_id("air")
 local blocks_per_chunk = tonumber(minetest.get_mapgen_setting("chunksize"))
 local chunk_side = blocks_per_chunk * 16
 
-minetest.register_on_mods_loaded(function()
-        local id_pairs = {}
-        for _, nodedef in pairs(minetest.registered_nodes) do
-            local name = nodedef.name
-            local id = minetest.get_content_id(name)
-            id_pairs[id] = id
-            placeholder_id_finder_pairs[id] = false
-        end
-        id_pairs[ignore_id] = false
-        placeholder_id_pairs = id_pairs
-end)
+-- iterate over ids of all possible existing nodes
+for i = 1, 32768 do
+    placeholder_id_finder_pairs[i] = false
+    placeholder_id_pairs[i] = i
+end
+
+placeholder_id_pairs[ignore_id] = false
 
 -- fun needs to be a function fun(pos1, pos2)
 -- where pos1 is minimal position in a mapchunk,
@@ -334,7 +330,6 @@ function ms.create_light_aware_top_placer(args)
         local replacement_id = minetest.get_content_id(replacement)
         replace_ids[find_id] = replacement_id
     end
-    
     return function(pos1, pos2)
         local pos_min, pos_max = pos1, pos2
         local vm = VoxelManip()
@@ -353,15 +348,12 @@ function ms.create_light_aware_top_placer(args)
                     local above = area:position(i)
                     above.y = above.y + 1
                     local above_index = area:indexp(above)
-                    if not data_light[above_index] then
-                        -- exit if at the edge of the chunk
-                        break
-                    end
                     local replacement = replace_ids[data[above_index]]
-                    if data_light[above_index] > higher_than and
+                    if data_light[above_index] and
+                        data_light[above_index] > higher_than and
                         data_light[above_index] < lower_than
                     then
-                        if chance >= math.random() then
+                        if chance >= math.random() and replacement then
                             data[above_index] = replacement
                             found = true
                         end
@@ -399,7 +391,6 @@ function ms.create_deco_finder(args)
                         ms.save_mapchunk(hash)
                         ms.handle_labels(hash, labels_to_add, labels_to_remove)
                         ms.add_labels(hash, {"scanned"})
-                        --minetest.log("error", dump(minp))
                     end
                 end
             end
