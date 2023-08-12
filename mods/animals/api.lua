7b75d881aff8796c6e92b8a9c47a66a38abc1019
TPH <DamotrixRob@gmail.com>
2023-08-12 05:30:44 -0400

    Increases 2x multiplier to 2.4x for animals/api.lua's harm by high temp

--- mods/animals/api.lua
+++ mods/animals/api.lua
@@ -190,7 +190,7 @@ function animals.core_life(self, lifespan, pos)
     energy = energy - math.random(4,8)
     
   -- get really hurt or die from high temp
-    if temp > self.max_temp * 2 then
+    if temp > self.max_temp * 2.4 then
        mobkit.hurt(self,math.ceil(3 * (temp / self.max_temp)))
        if (self.hp <= 0) then
          -- if animal successfully burned to death then
