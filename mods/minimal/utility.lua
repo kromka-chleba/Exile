a14049267a0c79f2fbc21a4a34c00180d1527ca4
TPH <DamotrixRob@gmail.com>
2023-08-13 21:39:21 +0000

    Adds "player_in_creative(plyr)" to minimal so that mods do not need to depend on the creative mod to check if a player is in creative. (#428)
    After seeing how many mods I would have to make depend on creative to do creative overrides on tools and stuff - especially mods that creative depended on - I decided to make a simple checker in minimal to decide whether or not a player is in creative instead of depending on the one provided in the creative mod for Exiles.
    
    This is a step I'm taking towards improving creative for v4 as I note in #425
    
    Reviewed-on: https://codeberg.org/Mantar/Exile/pulls/428
    Co-authored-by: TPH <DamotrixRob@gmail.com>
    Co-committed-by: TPH <DamotrixRob@gmail.com>

--- mods/minimal/utility.lua
+++ mods/minimal/utility.lua
@@ -246,6 +246,18 @@ function minimal.math_clamp(num,min,max) -- math.clamp implementation from my fu
 end
 
 
+-- check if a provided player is in creative mode
+local creative_mode_cache = minetest.settings:get_bool("creative_mode")
+function minimal.player_in_creative(plyr)
+  if (minetest.is_player(plyr)) then
+    if (minetest.check_player_privs(plyr,"creative") or creative_mode_cache == true) then
+      return true
+    end
+  end
+  
+  return false
+end
+
 
 
 -- Yes or no dialog
