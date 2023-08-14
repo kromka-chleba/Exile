d133ea2c0aee8cad872b2ca66036ebe67d935eea
Jeremy <jeremylshannon@gmail.com>
2023-08-14 14:47:19 -0700

    Add "unclaimable" group, prevents nailing down non-physical crafting spots

--- mods/minimal/protection.lua
+++ mods/minimal/protection.lua
@@ -15,7 +15,10 @@ function minimal.protection_nail_use( itemstack, user, pointed_thing )
 		if minimal.click_count_ready(owner, "nail", pt_pos, __nail_use_count) then
 			local pt_node=minetest.get_node(pt_pos)
 			if not (pt_node.name == 'tech:stick'
-				or minetest.get_item_group(pt_node.name, 'flora') > 0
+				or minetest.get_item_group(pt_node.name,
+							   'flora') > 0
+				or minetest.get_item_group(pt_node.name,
+							   'unclaimable') > 0
 			) then
 				local pt_meta=minetest.get_meta(pt_pos)
 				if not pt_meta:contains('owner') then
