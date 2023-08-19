86ab1a4449ee9b49d0bbf35612ea0b908cf8f359
Ceeee <ceeee@noreply.codeberg.org>
2023-08-19 19:57:52 +0000

    Add two missing help descriptions for chat commands, #340 (#456)
    In reference to #340 adding help descriptions for /restart and /respawn.
    
    Reviewed-on: https://codeberg.org/Mantar/Exile/pulls/456
    Co-authored-by: Ceeee <ceeee@noreply.codeberg.org>
    Co-committed-by: Ceeee <ceeee@noreply.codeberg.org>

--- mods/lore/restart.lua
+++ mods/lore/restart.lua
@@ -91,6 +91,7 @@ minetest.register_chatcommand("restart",{
 	privs = {
 		interact = true,
 	},
+        description = "Give up on your current character without leaving a trace. Everything you hold will be lost. Have a new Exile appear in this world in their stead and try yourself at survival again.",
 	func = restart
 })
 
@@ -98,6 +99,7 @@ minetest.register_chatcommand("respawn",{
 	privs = {
 		interact = true,
 	},
+        description = "This command is an alias for /restart. See there for further information.",
 	func = restart
 })
 
