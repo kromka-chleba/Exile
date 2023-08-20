7ac66eee33cff822826aeae4bb085283826e76f5
Jeremy <jeremylshannon@gmail.com>
2023-08-20 16:40:37 -0700

    Move invis to player_api; Cleanup and tutorial links in login.lua

--- mods/lore/login.lua
+++ mods/lore/login.lua
@@ -1,6 +1,8 @@
 --login.lua
 --A login screen to show to new players
 
+tutorial = tutorial
+
 local logintext = ( "  You can scarcely hear the sound of them "..
 		       "reading the list of your crimes over the " ..
 		       "louder jeering of your kinsmen, but it's already "..
@@ -18,31 +20,31 @@ local loginspec = ("formspec_version[3]"..
 		   "hypertext[0.5,0.75;6,5;introtext;"..logintext.."]"..
 		   "image[1.5,6;6,2;logo.png]" )
 
-local newplayer = {} -- visual_size doesn't work in on_newplayer, only in on_join
 
-minetest.register_on_joinplayer(function(player)
-      local name = player:get_player_name()
-      if newplayer[name] and newplayer[name] == true then
-	 local props = player:get_properties()
-	 -- hide new players until they read the intro (is_visible fails?)
-	 props.nametag = " "
-	 player:set_properties(props)
-	 player_monoids.visual_size:add_change(player,
-					       {x=0, y=0, z=0},
-					       "login:invis")
-	 newplayer[name] = false
-      end
-end)
+local newplayer = {} -- visual_size doesn't work in on_newplayer, only in on_join
 
 minetest.register_on_newplayer(function(player)
       newplayer[player:get_player_name()] = true
       minetest.show_formspec(player:get_player_name(),"lore:login",loginspec)
 end)
+-- process continues in on_joinplayer
+minetest.register_on_joinplayer(function(player)
+      local name = player:get_player_name()
+      if newplayer[name] and newplayer[name] == true then
+	 -- hide new players until they read the intro
+	 player_api.set_invisible(player, true)
+	 newplayer[name] = false
+      end
+end)
 
 local rspawn_available = false
+local tutorial_available = false
 for _, name in ipairs(minetest.get_modnames()) do
 	if name == "rspawn" then
 		rspawn_available = true
+	end
+	if name == "tutorial_exile" then
+		tutorial_available = true
 	end
 end
 
@@ -106,8 +108,11 @@ function play_themesong(name)
 end
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
-      --maybe unnecessary, but guarantee they won't be penalized for reading
       if formname == "lore:login" then
+	 if tutorial_available then
+	    tutorial.init(player)
+	 end
+	 -- Guarantee they won't be penalized for reading:
 	 reset_attributes(player) -- All stats back to starting values
 	 doGatewayFX(player)
 	 local pname = player:get_player_name()
@@ -121,13 +126,9 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 				       "/music/exile_theme.ogg")
 	    play_themesong(pname)
 	 end
-	 local props = player:get_properties()
-	 props.nametag = "" -- An empty tag defaults to player's name
 	 -- Bang! new player appears in the world
 	 minetest.after(0.25, function()
-			   player:set_properties(props)
-			   player_monoids.visual_size:del_change(player,
-								 "login:invis")
+			   player_api.set_invisible(player, false)
 	 end)
       end
 end)
