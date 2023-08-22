bb7a72c0cbbe15c67e8e08db245f6cb007e5b322
Jeremy <jeremylshannon@gmail.com>
2023-08-22 16:11:10 -0700

    Make login text easily translatable

--- mods/lore/login.lua
+++ mods/lore/login.lua
@@ -2,8 +2,11 @@
 --A login screen to show to new players
 
 tutorial = tutorial
+local S = minetest.get_translator("lore")
 
-local logintext = ( "  You can scarcely hear the sound of them "..
+local logintext = S("login_text")
+if string.match(logintext, "login_text") then -- Untranslated? Use default
+   logintext =       ( "  You can scarcely hear the sound of them "..
 		       "reading the list of your crimes over the " ..
 		       "louder jeering of your kinsmen, but it's already "..
 		       "too late to protest your innocence. "..
@@ -12,6 +15,7 @@ local logintext = ( "  You can scarcely hear the sound of them "..
 		       "punishment that is to be given, and then you "..
 		       "are pushed through a gateway to die in the "..
 		       "cursed land of the Ancients, as an.." )
+end
 
 local loginspec = ("formspec_version[3]"..
 		       "size[7,7.5]"..
