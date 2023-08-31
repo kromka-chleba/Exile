c81a1ad70118f93147acb5b05d359ed901cb3e08
TPH <DamotrixRob@gmail.com>
2023-08-31 08:11:44 -0400

    Addition of wooden pots for potash except for "dry_potash"

--- mods/tech/glassworking.lua
+++ mods/tech/glassworking.lua
@@ -280,7 +280,32 @@ liquid_store.register_stored_liquid(
 		}
 	},
 	S("Clay Water Pot with Potash Solution"),
-	{dig_immediate = 2})
+	{dig_immediate = 2, pottery = 1})
+-- solution in wooden pot
+liquid_store.register_stored_liquid(
+	"tech:potash_source",
+	"tech:wooden_water_pot_potash",
+	"tech:wooden_water_pot",
+	{
+		"tech_primitive_wood.png^tech_pot_empty.png^tech_pot_potash.png",
+		"tech_primitive_wood.png",
+		"tech_primitive_wood.png",
+		"tech_primitive_wood.png",
+		"tech_primitive_wood.png",
+		"tech_primitive_wood.png"
+	},
+	{
+		type = "fixed",
+		fixed = {
+			{-0.25, 0.375, -0.25, 0.25, 0.5, 0.25}, -- NodeBox1
+			{-0.375, -0.25, -0.375, 0.375, 0.3125, 0.375}, -- NodeBox2
+			{-0.3125, -0.375, -0.3125, 0.3125, -0.25, 0.3125}, -- NodeBox3
+			{-0.25, -0.5, -0.25, 0.25, -0.375, 0.25}, -- NodeBox4
+			{-0.3125, 0.3125, -0.3125, 0.3125, 0.375, 0.3125}, -- NodeBox5
+		}
+	},
+	S("Wooden Water Pot with Potash Solution"),
+	{dig_immediate = 2, pottery = 1})
 
 liquid_store.register_liquid("tech:potash_source", "tech:potash_flowing", false)
 
