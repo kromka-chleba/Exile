f88056f0239f706dfbb7d0588ff5a85c7e0c8b8c
Attila Hencz <attila.hencz.dev@gmail.com>
2023-09-02 11:26:13 +0200

    mods/minimal/init.lua: Fix indentation whitespace to be uniform
    To fix line-start misalignment for people not exactly having the same
    tab-length settings that would make space-indent and tab-indent on
    various lines line up.
    Making reading the code easier for everyone.
    
    Was predominantly tabs, so made it all tabs.
    
    Additionally, if there is a code-style rule to use space-indent only, now it should be
    the matter of 1 command to fix it.
    
    Also, removed empty line at EOF.

--- mods/minimal/init.lua
+++ mods/minimal/init.lua
@@ -40,11 +40,11 @@ function minimal.set_hotbar(player,pref)
 		-- use wide hotbar
 		player:hud_set_hotbar_image("gui_hotbar16.png")
 		player:hud_set_hotbar_itemcount(16)
-    minimal.update_quickslot(player,16)
+		minimal.update_quickslot(player,16)
 	else
 		player:hud_set_hotbar_image("gui_hotbar.png")
 		player:hud_set_hotbar_itemcount(8)
-    minimal.update_quickslot(player,8)
+		minimal.update_quickslot(player,8)
 	end
 end
 
@@ -124,15 +124,14 @@ minetest.register_item(":", {
 
 
 minetest.register_on_joinplayer(function(player)
-      local p_name = player:get_player_name()
-      --Custom small inventory
-      minetest.get_inventory({type="player", name=p_name}):set_size("main", 16)
-      --enable shadows if using minetest 5.6.0+
-      if minimal.mt_required_version(5,6,0) then
-	 minetest.log("action", "MT5.6.0+, enabling shadows for "..p_name)
-	 player:set_lighting({
-	    shadows = { intensity = 0.33 }
-      })
-      end
+	local p_name = player:get_player_name()
+	--Custom small inventory
+	minetest.get_inventory({type="player", name=p_name}):set_size("main", 16)
+	--enable shadows if using minetest 5.6.0+
+	if minimal.mt_required_version(5,6,0) then
+		minetest.log("action", "MT5.6.0+, enabling shadows for "..p_name)
+		player:set_lighting({
+			shadows = { intensity = 0.33 }
+		})
+	end
 end)
-
