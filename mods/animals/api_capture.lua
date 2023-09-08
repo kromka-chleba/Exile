03830bbf0f412c85f182a5b201bee6a078dbd48e
Jeremy <jeremylshannon@gmail.com>
2023-09-08 16:05:41 -0700

    Don't allow live-capturing of dead animals; fixes #520

--- mods/animals/api_capture.lua
+++ mods/animals/api_capture.lua
@@ -50,6 +50,7 @@ end
 
 --use stunning weapon plus chance to catch
 animals.stun_catch_mob = function(self, clicker,chance)
+	if self.hp <= 0 then return end
 	local item = clicker:get_wielded_item()
 	local item_name = item:get_name()
 	item = minetest.get_item_group(item_name,"club")
