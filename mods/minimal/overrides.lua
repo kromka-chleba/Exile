cd8f988c7ba9cd24d44396bde697bacbc5e90b38
Jeremy <jeremylshannon@gmail.com>
2023-09-14 20:58:54 -0700

    Don't add air to creative inventory

--- mods/minimal/overrides.lua
+++ mods/minimal/overrides.lua
@@ -6,7 +6,8 @@
 
 local fall_damage_multiplier = 1.5
 
-minetest.override_item("air", { groups = { air = 1} })
+minetest.override_item("air", { groups = { air = 1,
+					   not_in_creative_inventory = 1} })
 
 --A new item_place that allows disabling sneak-rightclick behavior for nodes
 --Needed for tech:stick
