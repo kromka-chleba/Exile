15c898a897f0c60bd79ac38300c0db9f79da762b
TPH <DamotrixRob@gmail.com>
2023-09-16 08:47:17 -0400

    Remove debug number for egg_timer

--- mods/animals/impethu.lua
+++ mods/animals/impethu.lua
@@ -222,7 +222,7 @@ local self_data = {
 -- energy and eggs
 self_data.energy_max = 5000   --secs it can survive without food
 self_data.energy_egg = self_data.energy_max/10  --energy that goes to egg
-self_data.egg_timer = 6--60*10
+self_data.egg_timer = 60*10
 self_data.young_per_egg = {2,4}		--will get this/energy_egg starting energy
 -- lifespan
 self_data.lifespan = self_data.energy_max * 4
