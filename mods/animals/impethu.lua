b03afc5615305227f57b6ce9695d31bd157b382e
TPH <DamotrixRob@gmail.com>
2023-09-16 08:21:56 -0400

    Rebase rebase rebase... xwx I don't get how this works but alright ok ugh git

--- mods/animals/impethu.lua
+++ mods/animals/impethu.lua
@@ -220,7 +220,7 @@ local self_data = {
 self_data.energy_max = 5000   --secs it can survive without food
 self_data.energy_egg = self_data.energy_max/10  --energy that goes to egg
 self_data.egg_timer = 60*10
-self_data.young_per_egg = 5		--will get this/energy_egg starting energy
+self_data.young_per_egg = {2,4}		--will get this/energy_egg starting energy
 -- lifespan
 self_data.lifespan = self_data.energy_max * 4
 ---------------------;
--- mods/animals/kubwakubwa.lua
+++ mods/animals/kubwakubwa.lua
@@ -228,7 +228,7 @@ local self_data = {
 self_data.energy_max = 8000   --secs it can survive without food
 self_data.energy_egg = self_data.energy_max/2  --energy that goes to egg
 self_data.egg_timer = 60*35
-self_data.young_per_egg = 4   --will get this/energy_egg starting energy
+self_data.young_per_egg = {3,4}   --will get this/energy_egg starting energy
 self_data.cn_min = (self_data.energy_egg / self_data.young_per_egg) * 0.4 -- conserve min (minimum point at when to conserve energy)
 -- lifespan
 self_data.lifespan = self_data.energy_max * 6
