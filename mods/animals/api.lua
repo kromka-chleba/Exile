73cd503611ca29738778e9460031c2041b264f27
TPH <DamotrixRob@gmail.com>
2023-09-17 11:45:00 -0400

    Animals now lose energy proportionate to the temperature (as well as a base -4). Pegasun max_temp set up to 44. Darkasthaan min_temp set up to 14

--- mods/animals/api.lua
+++ mods/animals/api.lua
@@ -264,7 +264,13 @@ function animals.core_life(self, lifespan, pos)
       conserve = false -- moving around, thus not conserving energy
     end
     -- lose energy from discomfort
-    energy = energy - math.random(4,8)
+    energy = energy - 4
+    -- lose more energy dependent on temperature difference (discomfort also)
+    if (temp > self.max_temp) then
+      energy = energy - abs(temp - self.max_temp)
+    elseif (temp < self.min_temp) then
+      energy = energy - abs(temp - self.min_temp)
+    end
     
     
     
