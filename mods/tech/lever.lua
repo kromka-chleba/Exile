b125df8e9bea002395e770e0b9680b350a313e9e
TPH <DamotrixRob@gmail.com>
2023-09-18 23:37:43 -0400

    tech/lever now properly utilizes minimal.player_in_creative()

--- mods/tech/lever.lua
+++ mods/tech/lever.lua
@@ -215,8 +215,7 @@ local function aligner(itemstack, user, pointed_thing, grab)
       if should_rotate(ndef, node, pos, itemstack, user, new_param2) then
 	 node.param2 = new_param2
 	 minetest.swap_node(pos, node) -- and set it
-	 if not (creative and creative.is_enabled_for and
-		 creative.is_enabled_for(player_name)) then
+	 if not minimal.player_in_creative(user) then
 	    itemstack:add_wear(65535 / ((ndef._uses or 200) - 1))
 	 end
       end
