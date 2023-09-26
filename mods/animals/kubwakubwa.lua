1ce4ca0ce9dbb17e02515515a4987e7f05e0f6fd
TPH <DamotrixRob@gmail.com>
2023-09-26 14:02:04 -0400

    Lowers emergency_egg_chance for sneachan and kubwakubwa

--- mods/animals/kubwakubwa.lua
+++ mods/animals/kubwakubwa.lua
@@ -219,7 +219,7 @@ self_data.energy_max = 8000   --secs it can survive without food
 self_data.energy_egg = self_data.energy_max/2  --energy that goes to egg
 self_data.egg_timer = 60*20
 self_data.young_per_egg = {3,4}   --will get this/energy_egg starting energy
-self_data.emergency_egg_chance = 0.95
+self_data.emergency_egg_chance = 0.85
 self_data.cn_min = (self_data.energy_egg / self_data.young_per_egg[2]) * 0.4 -- conserve min (minimum point at when to conserve energy)
 -- lifespan
 self_data.lifespan = self_data.energy_max * 6
