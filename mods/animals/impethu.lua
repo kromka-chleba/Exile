f7256da2e8a60788619bf33086760b805250ed3c
TPH <DamotrixRob@gmail.com>
2023-09-26 15:51:18 -0400

    Modifications to impethu place_egg chance and condition

--- mods/animals/impethu.lua
+++ mods/animals/impethu.lua
@@ -92,9 +92,9 @@ local function brain(self)
 
 			--reproduction
 			--asexual parthogenesis, eggs
-			if random() < 0.005 then
+			if random() < 0.008 then
 				if not rival
-				and energy >= (self.energy_max * 0.99) then
+				and energy >= (self.energy_max * 0.95) then
 					energy = animals.place_egg(self, pos, energy)
 				end
 			end
