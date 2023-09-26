97ae9927d74b7633e2eadb6ae43add6c88f6df32
TPH <DamotrixRob@gmail.com>
2023-09-26 15:53:54 -0400

    Increases emergency egg chance for sneachan

--- mods/animals/sneachan.lua
+++ mods/animals/sneachan.lua
@@ -240,7 +240,7 @@ self_data.energy_max = 5000--secs it can survive without food
 self_data.energy_egg = (self_data.energy_max*0.5) --energy that goes to egg
 self_data.egg_timer = 60*10
 self_data.young_per_egg = {3,7}		--will get this/energy_egg starting energy
-self_data.emergency_egg_chance = 0.6
+self_data.emergency_egg_chance = 0.75
 -- lifespan
 self_data.lifespan = self_data.energy_max * 5
 ---------------------;
