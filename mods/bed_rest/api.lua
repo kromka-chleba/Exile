26e5541553c997db59b03de37f9925381e2972df
TPH <DamotrixRob@gmail.com>
2023-10-01 07:42:12 -0400

    Just replaces "minetest.swap_node(p,{name = "air"})" with "minetest.remove_node(p)", does not affect metadata saving

--- mods/bed_rest/api.lua
+++ mods/bed_rest/api.lua
@@ -155,7 +155,8 @@ function bed_rest.register_bed(name, def)
 			end
 			node.param2 = new_param2
 			-- do not remove_node here - it will trigger destroy_bed()
-			minetest.swap_node(p, {name = "air"})
+			--minetest.swap_node(p, {name = "air"})
+      minetest.remove_node(p)
 			minetest.swap_node(pos, node)
 			minetest.swap_node(newp, {name = name .. "_top", param2 = new_param2})
 			return true
