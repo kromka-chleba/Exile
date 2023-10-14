0c95e0d2af8de62a863c306e9fd201c003127f22
Jeremy <jeremylshannon@gmail.com>
2023-10-13 21:00:44 -0700

    Create interface/hotbar.lua in minimal

--- mods/minimal/interface/init.lua
+++ mods/minimal/interface/init.lua
@@ -2,5 +2,6 @@ local modpath=minetest.get_modpath('minimal').."/interface"
 dofile(modpath..'/item_names.lua')
 dofile(modpath..'/infotext.lua')
 dofile(modpath..'/themes.lua')
+dofile(modpath..'/hotbar.lua') -- uses themes, keep it below that
 dofile(modpath..'/minimal_hud.lua') -- uses math_clamp from utility/
 dofile(modpath..'/tooltips.lua')
