7e9b747011a790a24924d332cbba0247a7cc7423
Jeremy <jeremylshannon@gmail.com>
2023-10-29 17:24:12 -0700

    Disallow bed placement on strange things

--- mods/bed_rest/api.lua
+++ mods/bed_rest/api.lua
@@ -32,7 +32,12 @@ local function is_invalid_pos(pos, player_name)
 
    local node_def = minetest.registered_nodes[minetest.get_node(pos).name]
    if not node_def or not node_def.buildable_to
-      or string.match(node_def.drawtype, "liquid") then
+      or string.match(node_def.drawtype, "liquid")
+      or node_def.drawtype == "plantlike"
+      or node_def.drawtype == "signlike"
+      or node_def.drawtype == "torchlike"
+      or node_def.drawtype == "firelike"
+   then
       return true
    end
    return false
