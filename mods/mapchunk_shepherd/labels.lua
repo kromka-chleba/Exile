a3abb0425f0a8ec422b5845c78fcd9e7cab18a54
Jeremy <jeremylshannon@gmail.com>
2023-11-17 17:48:08 -0800

    Don't crash if labels are invalid

--- mods/mapchunk_shepherd/labels.lua
+++ mods/mapchunk_shepherd/labels.lua
@@ -111,11 +111,11 @@ local function get_paired_labels(labels)
     local paired = {}
     for _, label in pairs(labels) do
         local name = label[1]
-        -- if not name then
-        --     minetest.log("error", dump(labels))
-        -- else
-            paired[name] = label
-            --end
+        if not name then
+	   minetest.log("error", dump(labels))
+	else
+	   paired[name] = label
+	end
     end
     return paired
 end
