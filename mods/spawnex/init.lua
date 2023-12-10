0522a85b989aa77112c5183a79e3c5b7806d2d56
Jeremy <jeremylshannon@gmail.com>
2023-12-09 16:28:41 -0800

    Try not to use fallback_spawn_pos, and if we do, ensure players aren't buried

--- mods/spawnex/init.lua
+++ mods/spawnex/init.lua
@@ -43,9 +43,10 @@ local dirstring, hexnum,
 
 local defhex = {0,0}
 
-local function fallback_spawn_pos(pos)
+local function fallback_spawn_pos(hex)
    -- Used if we have to fallback to the hex center for some reason, eg volcano
    pirnt("fallback_spawn_pos")
+   local pos = hex2map(hex)
    local vground = volcano.estimate_ground_level(pos)
    local vgl = vground or 20
    local tries = 0
@@ -80,7 +81,7 @@ local function spawn_offset(hex1, max_count)
    -- Gets a random spot in a circular field around the center of the region
    local hexctr = hex2map(hex1) -- get the center
    local tgt, d, sl, vgl
-   local slcount, biomecount = 0
+   local slcount, biomecount = 0, 0
    local count = 0 local maxcount = max_count or 20
    local axis = vector.new(0,1,0)
    repeat
@@ -91,27 +92,32 @@ local function spawn_offset(hex1, max_count)
       local check = vector.round(hexctr:add(
 				    vector.rotate_around_axis(dist,axis,angle)))
       sl = minetest.get_spawn_level(check.x, check.z)
-      vgl = volcano.estimate_ground_level(check) or 0
-      local chunk = ms.mapchunk_hash(check)
-      local bname =
-	 minetest.get_biome_name(minetest.get_biome_data(
-				    vector.new(check.x,
-					       sl or 19,
-					       check.z)).biome)
-      if not sl then slcount = slcount + 1 end
-      if not isagoodbiome(bname) then biomecount = biomecount + 1 end
-      if (sl and sl > vgl) and -- not at a volcano
-	 not ms.contains_labels(chunk, badplaces) and -- not a blacklisted label
-	 isagoodbiome(bname) then -- not a blacklisted biome name
-	 tgt = check
-      end
+      if not sl then
+	 slcount = slcount + 1
+	 else
+	    vgl = volcano.estimate_ground_level(check) or 0
+	    local chunk = ms.mapchunk_hash(check)
+	    local bname =
+	       minetest.get_biome_name(minetest.get_biome_data(
+					  vector.new(check.x,
+						     sl or 19,
+						     check.z)).biome)
+	    if not isagoodbiome(bname) then biomecount = biomecount + 1 end
+	    if  sl > vgl and -- not at a volcano
+	       not ms.contains_labels(chunk, badplaces)
+	       -- not a blacklisted label
+	       and  isagoodbiome(bname) then -- not a blacklisted biome name
+	       tgt = check
+	    end
+	 end
       count = count + 1
-   until count > maxcount or tgt
+   until count >= maxcount or tgt
    if tgt then
       tgt.y = sl
    end -- apply spawn level, or return nil
-   pirnt("spawn_offset ",hex2string(hex1),(tgt ~= nil)," count: ",count,
-	 slcount, biomecount)
+   pirnt("spawn_offset ",hex2string(hex1),
+	 (tgt == nil and " NOT" or "    ")," FOUND - ",
+	 "tries/sl/biome: ", count,"/",slcount,"/",biomecount)
    return tgt
 end
 
@@ -155,7 +161,8 @@ end
 local function find_gate_pos(hex, tries) -- pick a spawn position in a hex
    local gate = spawn_offset(hex, tries or 350)
    if not gate then
-      gate = fallback_spawn_pos(hex2map(hex))
+      pirnt("bad gate for ",hex2string(hex))
+      add_job("setup", 1, hex)
    end
    return gate
 end
@@ -176,12 +183,14 @@ local function load_gate(hex) -- forceload a gate's location to prep for a spawn
 end
 
 local function setup_gate(hex) -- create potential gate
-   pirnt("setup gate for ",hex2string(hex))
    local def = region.get(hex)
    if not def.currentgate then
+      pirnt("setup new gate for ",hex2string(hex))
       def.currentgate = find_gate_pos(hex)
+      save_rgns()
+      return true
    end
-   save_rgns()
+   return false
 end
 
 function queue_next_gate(hex) -- Set up next gate before closing current one
@@ -200,8 +209,11 @@ function queue_next_gate(hex) -- Set up next gate before closing current one
 end
 
 function region.fast_gate(hex) -- No gate for this hex, make one quick!
+   local gate = find_gate_pos(hex, 25) -- just 25 tries before giving up
+   if not gate then
+      gate = fallback_spawn_pos(hex)
+   end
    local def = region.get(hex)
-   local gate = find_gate_pos(hex, 5) -- just 5 tries before giving up
    def.currentgate = gate
    save_rgns()
    return gate
@@ -282,6 +294,24 @@ function region.prespawn(player, centrhx) -- Ready a spawn gate for this player
    meta:set_string("exile_spawnat", hex2string(tgt))
 end
 
+local function fixplayer(player)
+   if not minetest.is_player(player) then return end
+   pirnt("Fixplayer needed for "..player:get_player_name() )
+   local pos = player:get_pos()
+   local node = minetest.get_node(pos)
+   if node.name == "ignore" then
+      minetest.after(0.1, fixplayer, player)
+      return
+   end
+   local light = minimal.get_daylight(pos, 0.5)
+   if light < 6 then
+      player:set_pos(vector.new(pos.x, pos.y + 5, pos.z))
+      minetest.after(0.1, fixplayer, player)
+      return
+   end
+   return -- light > 5 indicates we're probably not underground
+end
+
 function region.spawn(player)
    pirnt("region spawn")
    local meta = player:get_meta()
@@ -294,17 +324,20 @@ function region.spawn(player)
 
    local sadef = region.get(spawnat)
    local gate = sadef.currentgate
+   local guessed_gate = false
    if not gate then
       gate = region.fast_gate(spawnat)
       sadef.currentgate = gate
       load_gate(spawnat)
+      guessed_gate = true
    end
    sadef.open = true
    pirnt("spawn: ",dump(sadef.currentgate))
    player:set_pos(gate)
+   if guessed_gate then fixplayer(player) end
    -- get a new spawn location, but wait until this gate is closed!
    minetest.after(70, function() region.prespawn(player) end )
-   add_job("queue", 50, spawnat)
+   add_job("queue", 20, spawnat)
    add_job("close", 60, spawnat)
    save_jobs()
 end
@@ -318,7 +351,8 @@ local homecache = {}
 local savejobs = false
 
 local func = -- can't serialize actual functions, so correlate with string name
-   { ["queue"] = queue_next_gate, ["close"] = close_gate }
+   { ["queue"] = queue_next_gate, ["close"] = close_gate,
+      ["setup"] = setup_gate, }
 
 -- how far you need to get from center of home to new home
 local maxdist = 2750 -- 2000 to the edge of the next hex, 1250 from its center
@@ -355,7 +389,6 @@ local function player_moved_to_new_region(player, pname, ppos, home)
    end
    return saveout
 end
-
 
 minetest.register_globalstep(function(dtime)
       for nm, dat in pairs(jobs) do -- run jobs
@@ -510,11 +543,10 @@ minetest.register_chatcommand("hexport",{
 	      return false, "Invalid parameters, must be <hex #, x> <hex #, z>"
 	   end
 	   local ppos = minetest.get_player_by_name(name):get_pos():round()
-	   local hexpos = hex2map(tgt, ppos.y)
 	   local spos = spawn_offset(tgt)
 	   if spos == nil then return false, "can't find a valid pos" end
 	   minetest.get_player_by_name(name):set_pos(spos)
-	   return true, minetest.pos_to_string(hexpos).." -> "..
+	   return true, minetest.pos_to_string(ppos).." -> "..
 	      minetest.pos_to_string(spos)
 	end
 })
@@ -531,8 +563,6 @@ minetest.register_chatcommand("spawnlevel",{
 	   return true, sl
 	end
 })
-
-end
 
 minetest.register_chatcommand("biome",{
 	--privs = "server",
@@ -574,3 +604,53 @@ minetest.register_chatcommand("myhex",{
 				  or "disabled")
 	end
 })
+
+minetest.register_chatcommand("newgate",{
+	--privs = "server",
+	func = function(name,param)
+	   local hex = string2hex(param)
+	   if not hex then
+	      return false, "Invalid parameters, must be <hex #, x> <hex #, z>"
+	   end
+	   close_gate(hex)
+	   setup_gate(hex)
+	   local out =  "Set up new gate for "..hex2string(hex).." at "
+	   local pos = region.get(hex).currentgate
+	   out = out .. minetest.pos_to_string(pos)
+	   return true, out
+	end
+})
+
+minetest.register_chatcommand("fbspawn",{
+	--privs = "server",
+	func = function(name,param)
+	   local tgt = string2hex(param)
+	   if not tgt then
+	      return false, "Invalid parameters, must be <hex #, x> <hex #, z>"
+	   end
+	   local player = minetest.get_player_by_name(name)
+	   local ppos = player:get_pos():round()
+	   local spos = fallback_spawn_pos(tgt)
+	   if spos == nil then return false, "can't find a valid pos" end
+	   player:set_pos(spos)
+	   return true, minetest.pos_to_string(ppos).." -> "..
+	      minetest.pos_to_string(spos)
+	end
+})
+
+minetest.register_chatcommand("fixplayer",{
+	--privs = "server",
+	func = function(name,param)
+	   local player
+	   if param then
+	      player = minetest.get_player_by_name(param)
+	   end
+	   if not player then
+	      player = minetest.get_player_by_name(name)
+	   end
+	   fixplayer(player)
+	end
+})
+
+end
+
