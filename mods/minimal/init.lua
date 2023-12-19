f35d54b9db860e90bb6c7f96bea4030ca9b75e87
Jeremy <jeremylshannon@gmail.com>
2023-12-18 23:11:11 -0800

    Add a /ping command to reveal your location to other players on a server

--- mods/minimal/init.lua
+++ mods/minimal/init.lua
@@ -32,6 +32,7 @@ dofile(modpath..'/metadata.lua')
 dofile(modpath..'/triggers.lua')
 dofile(modpath..'/zones.lua')
 dofile(modpath..'/interface/init.lua')
+dofile(modpath..'/player.lua')
 
 dofile(modpath..'/currentrevision.lua')
 dofile(modpath..'/storage_api.lua')
