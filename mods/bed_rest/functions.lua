ed53f154a2d5599b41b9b8b3080aa1feeb64806c
Izzy Blacklock <izzyb@planetofnix.com>
2023-12-28 21:13:11 +0000

    Bug Fix - infotext on beds overwritten instead of merged (#716)
    Bug fix.  Issue #562
    
    Reviewed-on: https://codeberg.org/Mantar/Exile/pulls/716
    Co-authored-by: Izzy Blacklock <izzyb@planetofnix.com>
    Co-committed-by: Izzy Blacklock <izzyb@planetofnix.com>

--- mods/bed_rest/functions.lua
+++ mods/bed_rest/functions.lua
@@ -224,7 +224,7 @@ local function wear_blanket(player, bed_pos, donning)
 		end
    end
    if not bedInv:is_empty('main') then
-	   bed_meta:set_string('infotext','Bed: Contains Blanket')
+      minimal.infotext_merge(bed_pos, 'Bed: Contains Blanket', bed_meta)
    end
    clothing:update_temp(player)
    player_api.set_texture(player)
