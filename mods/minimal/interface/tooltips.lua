04ccb0b4fa62aa0f114a6a60f37f7782cc4e7ae8
Izzy Blacklock <izzyb@planetofnix.com>
2023-12-30 20:38:19 -0700

    Updated tooltips to save as def._tool_tips instead of modifying description
    Desription set via metadata in crafting/api.lua when items are crafted instead.
    Does away with then to track old description; original description can be pulled from
    the definintion and modified description on the item from the item meta data.
    mouseover is updated using metedata if found.
    
    Was needed to add creator to tools and keys in their description.

--- mods/minimal/interface/tooltips.lua
+++ mods/minimal/interface/tooltips.lua
@@ -1,29 +1,31 @@
+-- auto generate _tool_tips for all registered items defining  _dig_tip, _use_tip, or _place_tip
+-- Stored in def._tool_tips -- crafting/api.lua sets meta description using it which appears with mouseover.
 minetest.register_on_mods_loaded(function()
-      for name, def in pairs(minetest.registered_items) do
-	 if def._orig_desc then
-	    minetest.log("error","Tried to set tooltips on "..name.." twice!")
-	 else
-	    local orig_desc = def.description
-	    local newdesc = def.description
-	    local ttip = ""
-	    local digtip = def._dig_tip
-	    local usetip = def._use_tip
-	    local placetip = def._place_tip
-	    if digtip or usetip or placetip then
-	       ttip = ttip.."\n"
-	       if digtip then
-		  ttip = ttip.."\n  ^ : "..digtip
-	       end
-	       if usetip then
-		  ttip = ttip.."\n  ◊ : "..usetip
-	       end
-	       if placetip then
-		  ttip = ttip.."\n  v : "..placetip
-	       end
-	       newdesc = newdesc..minetest.colorize("#ccccff", ttip)
-	       minetest.override_item(name, { description = newdesc,
-					      _orig_desc = orig_desc })
-	    end
-	 end
-      end
+	for name, def in pairs(minetest.registered_items) do
+		if def._orig_desc then
+			minetest.log("error","Tried to set tooltips on "..name.." twice!")
+		else
+			local ttip = ""
+			local digtip = def._dig_tip
+			local usetip = def._use_tip
+			local placetip = def._place_tip
+			if digtip or usetip or placetip then
+				ttip = ttip.."\n"
+				if digtip then
+					ttip = ttip.."\n  ^ : "..digtip
+				end
+				if usetip then
+					ttip = ttip.."\n  ◊ : "..usetip
+				end
+				if placetip then
+					ttip = ttip.."\n  v : "..placetip
+				end
+				minetest.override_item(name, { 
+					_tool_tips=minetest.colorize("#ccccff", ttip)
+				})
+
+			end
+		end
+	end
 end)
+
