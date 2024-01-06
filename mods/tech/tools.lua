3e76edf4910bac436d703614336cda5b7fb4007c
Izzy Blacklock <izzyb@planetofnix.com>
2024-01-06 11:18:37 -0700

    Moved crafting of stations to hand_create tab in response to dokimi's post to issue #138

--- mods/tech/tools.lua
+++ mods/tech/tools.lua
@@ -97,7 +97,7 @@ local open_chopping_spot = {
     crafting.make_on_rightclick({"axe","axe_mixing"}, 2, { x = 8, y = 3 }),
 }
 
-local open_knife = crafting.make_on_rightclick({"knife",'knife_stations','knife_wattle','knife_mixing'}, 2, { x = 8, y = 3 })
+local open_knife = crafting.make_on_rightclick({"knife",'knife_wattle','knife_mixing'}, 2, { x = 8, y = 3 })
 
 -- checks if the node has one of the groups from good_on
 local function is_spot_valid(node, good_on)
