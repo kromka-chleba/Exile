11d1a5926765bb36dbb33c3d7be978dc8760f817
Jeremy <jeremylshannon@gmail.com>
2024-02-08 10:25:34 -0800

    waypoints: add a nil check, because minetest.after is bad

--- mods/minimal/utility/player.lua
+++ mods/minimal/utility/player.lua
@@ -39,6 +39,7 @@ end
   end
 
   local function clear_waypoint(name)
+     if not waypoints[name] then return end
      for _, data in pairs(waypoints[name]) do
         if minetest.is_player(data.obj) and data.handle then
      data.obj:hud_remove(data.handle)
