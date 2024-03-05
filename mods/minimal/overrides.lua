1be56483fdab6dd68297be447917e2447c7d337d
Jeremy <jeremylshannon@gmail.com>
2024-03-04 23:13:43 -0800

    minimal/overrides: clear all luacheck warnings

--- mods/minimal/overrides.lua
+++ mods/minimal/overrides.lua
@@ -4,6 +4,8 @@
 --is_protected
 --fall damage
 
+minetest = minetest
+
 local fall_damage_multiplier = 1.5
 
 minetest.override_item("air", { groups = { air = 1,
@@ -11,14 +13,18 @@ minetest.override_item("air", { groups = { air = 1,
 
 --A new item_place that allows disabling sneak-rightclick behavior for nodes
 --Needed for tech:stick
-function core.item_place(itemstack, placer, pointed_thing, param2)
+function minetest.item_place(itemstack, placer, pointed_thing, param2)
         -- Call on_rightclick if the pointed node defines it
         if pointed_thing.type == "node" and placer then
                 local node = core.get_node( pointed_thing.under )
                 local ndef = core.registered_nodes[ node.name ]
 
-                if ndef and ndef.on_rightclick and ( ndef.override_sneak == true or not placer:get_player_control( ).sneak ) then
-                        return ndef.on_rightclick( pointed_thing.under, node, placer, itemstack, pointed_thing ) or itemstack, nil
+                if ndef and ndef.on_rightclick and
+		   ( ndef.override_sneak == true or
+		     not placer:get_player_control( ).sneak ) then
+		   return ndef.on_rightclick( pointed_thing.under,
+					      node, placer, itemstack,
+					      pointed_thing ) or itemstack, nil
                 end
         end
 
@@ -34,8 +40,7 @@ local old_is_protected = minetest.is_protected
 function minetest.is_protected(pos, name)
    local owner = minetest.get_meta(pos):get_string("owner")
    local bypass = minetest.check_player_privs(name, "protection_bypass")
-   if not ( owner == "" or owner == name or
-	    minetest.check_player_privs(name, "protection_bypass") ) then
+   if not ( owner == "" or owner == name or bypass ) then
       return true
    end
    return old_is_protected(pos, name)
