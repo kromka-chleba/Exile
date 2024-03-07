5feba07bc52f0009779d51082ef685edcb969f90
Jeremy <jeremylshannon@gmail.com>
2024-03-06 22:31:54 -0800

    letter: add Seneschal, fix typo

--- mods/lore/exile_letter.lua
+++ mods/lore/exile_letter.lua
@@ -46,11 +46,12 @@ local judger = {
   "Council",
   "Courts",
   "Presidium",
-  "Law Giver's",
+  "Law Givers",
   "King's Justice",
   "Sheriff",
   "Exarch",
   "Prefect",
+  "Seneschal",
   --cults
   "Brotherhood",
   "Sisterhood",
