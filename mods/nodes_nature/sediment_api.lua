319b8596eafb6a2a02c6126983f4f259b66330c8
TPH <DamotrixRob@gmail.com>
2024-03-28 23:56:33 +0000

    Hard Winter Soils + Sediment Translation Template Fixing Fixes #615 (#777)
    As the title implies :D
    
    Clay soils are hardstone-hard as Meniptah argued should be the case (cracky=2, no crumbly)
    Other soils are cracky=3 and crumbly=1
    
    Also applies lord_of_the_dumpster's suggestion of "Frosty" being added to "Frosty Winter" (https://codeberg.org/Mantar/Exile/issues/395#issuecomment-1102870)
    
    Reviewed-on: https://codeberg.org/Mantar/Exile/pulls/777
    Co-authored-by: TPH <DamotrixRob@gmail.com>
    Co-committed-by: TPH <DamotrixRob@gmail.com>

--- mods/nodes_nature/sediment_api.lua
+++ mods/nodes_nature/sediment_api.lua
@@ -484,9 +484,18 @@ end
 function soil.get_winter_props(soil_desc)
     local sed = soil_desc.sediment
     local props = table.copy(soil.get_dry_node_props(soil_desc))
-    props.description = S("Winter @1", soil_desc.description)
+    props.description = S("Frosty Winter @1", soil_desc.description)
     props.groups.spreading = nil
     props.groups.winter_soil = 1
+    if props.groups.sediment == 2 then -- clay
+      props.groups.crumbly = nil
+      props.groups.cracky = 2
+    else -- sand, gravel, silt
+      if props.groups.crumbly then
+        props.groups.crumbly = 1
+      end
+      props.groups.cracky = 3
+    end
     props.tiles = {soil.get_winter_texture_name(soil_desc.name, sed.name),
                    sediment.get_dry_texture_name(sed.name),
                    {name = soil.get_winter_side_texture_name(soil_desc.name, sed.name)}}
@@ -512,7 +521,7 @@ function soil.register_winter_wet(soil_desc)
 	local winter_wet_name = soil.get_winter_wet_name(soil_desc.name)
 	local props = table.copy(soil.get_winter_props(soil_desc))
 	local sed = soil_desc.sediment
-	props.description = S("Winter Wet @1", soil_desc.description)
+	props.description = S("Frosty Winter Wet @1", soil_desc.description)
 	props.groups.spreading = nil
 	props.groups.winter_soil = 1
 	props.tiles = {
