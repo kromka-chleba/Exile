c7b0455e6b6074f25531dab1474150e7dbdf4d00
TPH <DamotrixRob@gmail.com>
2024-04-05 06:29:53 -0400

    Moves "pitches" table outside of on_construct function and renames it to "singing_pitches" instead of recreating the table every on_construct call

--- mods/artifacts/art.lua
+++ mods/artifacts/art.lua
@@ -580,7 +580,15 @@ minetest.register_node("artifacts:singing_stone", {
 })
 
 
-
+local singing_pitches = { -- old notes appeared to be G4, D#4, C4, A4/A#4 (in order)
+  195.117/260, -- G3 (195.117hz)
+  1, -- C4 (260hz)
+  294.870/260, -- D4 (294.870hz)
+  310.238/260, -- D#4 (310.238hz)
+  348.697/260, -- F4 (348.697hz)
+  391.259/260, -- G4 (391.259hz)
+  469.203/260, -- A#4 (469.203hz)
+}
 minetest.register_node("artifacts:singing_stone_b", {
 	description = S("Singing Stone"),
 	tiles = {"artifacts_sun_stone.png"},
@@ -608,17 +616,8 @@ minetest.register_node("artifacts:singing_stone_b", {
 	on_construct = function(pos)
 		--sing, and start timer to turn off
 		minetest.get_node_timer(pos):start(1)
-    local pitches = { -- old notes appeared to be G4, D#4, C4, A4/A#4 (in order)
-      195.117/260, -- G3 (195.117hz)
-      1, -- C4 (260hz)
-      294.870/260, -- D4 (294.870hz)
-      310.238/260, -- D#4 (310.238hz)
-      348.697/260, -- F4 (348.697hz)
-      391.259/260, -- G4 (391.259hz)
-      469.203/260, -- A#4 (469.203hz)
-    }
 		minimal.sound_play({name="artifacts_singing_stone", pos = pos, gain = {0.1,0.3},
-      pitch = pitches[math.random(#pitches)],max_hear_distance = 8})
+      pitch = singing_pitches[math.random(#singing_pitches)],max_hear_distance = 8})
 	end,
 
 	on_timer =function(pos, elapsed)
