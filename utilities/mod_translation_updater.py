8fd23e55485e05d1509c7767354d97434ed36c38
tour <yl_tour@posteo.de>
2024-05-22 20:22:48 +0200

    Extend the regex to detect more translation Strings

--- utilities/mod_translation_updater.py
+++ utilities/mod_translation_updater.py
@@ -124,17 +124,17 @@ def main():
 # Group 2 will be the string, groups 1 and 3 will be the delimiters (" or ')
 # See https://stackoverflow.com/questions/46967465/regex-match-text-in-either-single-or-double-quote
 pattern_lua_quoted = re.compile(
-	r'(?:^|[\.=,{\(\s])' # Look for beginning of file or anything that isn't a function identifier
+	r'(?:^|[\W])' # Look for beginning of file or anything that isn't a function identifier
 	r'N?F?S\s*\(\s*' # Matches S, FS, NS or NFS function call
-	r'(["\'])((?:\\\1|(?:(?!\1)).)*)(\1)' # Quoted string
-	r'[\s,\)]', # End of call or argument
+	r'(["\'])((?:\\\1|(?:(?!\1)).)*)(\1)', # Quoted string
+	# r'[\s,\)]', # End of call or argument
 	re.DOTALL)
 # Handles the [[ ... ]] string delimiters
 pattern_lua_bracketed = re.compile(
-	r'(?:^|[\.=,{\(\s])' # Same as for pattern_lua_quoted
+	r'(?:^|[\W])' # Same as for pattern_lua_quoted
 	r'N?F?S\s*\(\s*' # Same as for pattern_lua_quoted
-	r'\[\[(.*?)\]\]' # [[ ... ]] string delimiters
-	r'[\s,\)]', # Same as for pattern_lua_quoted
+	r'\[\[(.*?)\]\]', # [[ ... ]] string delimiters
+	# r'[\s,\)]', # Same as for pattern_lua_quoted
 	re.DOTALL)
 
 # Handles "concatenation" .. " of strings"
