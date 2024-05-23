79453977a4e712d5799f396b3f02d003a11a6122
TPH <DamotrixRob@gmail.com>
2024-05-23 04:35:27 -0400

    Removes fermentation functionality from tech/drugs.lua in favour of ncrafting/fermentation.lua - modifies functions accordingly

--- mods/tech/drugs.lua
+++ mods/tech/drugs.lua
@@ -9,9 +9,8 @@ local S = tech.S
 
 local random = math.random
 
--- BASIC FUNCTIONALITIES
-
-local ferment_interval = 5
+-- Globals
+ncrafting = ncrafting
 
 -----------------------------------
 --MEDICAL
@@ -165,91 +164,6 @@ liquid_store.register_stored_liquid("tech:wooden_tang",{
 -----------------
 -- UNFERMENTED TANG
 
-local function get_ferment_data(name)
-  if type(name) == "userdata" and  type(name["get_name"]) == "function" then
-    name = name:get_name()
-  elseif type(name) == "table" then
-    if name.x and name.y and name.z then
-      name = minetest.get_node(name)
-    end
-    name = name.name
-  end
-  if type(name) ~= "string" then return end
-  local def = minetest.registered_nodes[name]
-  if not def then return end
-  return {
-    to = def._ferment_to,
-    time = def._ferment_time or {min=300,max=360},
-    temp_range = def._ferment_temp_range
-  }
-end
-
--- find ferment or create a ferment meta
-local function get_or_create_ferment(name,meta)
-  local ferment = type(meta) == "userdata" and meta:get_int("ferment") or 0
-  if (ferment == 0) then
-    ferment = math.random(300,360) -- base to return if error
-    local ferment_data = get_ferment_data(name)
-    if not ferment_data then return ferment end
-    ferment = type(ferment_data.time) == "number" and ferment_data.time
-      or type(ferment_data.time) == "table" and math.random(ferment_data.time.min,ferment_data.time.max) or ferment
-  end
-  return ferment
-end
-
---set saved
-local after_place_ferment = function(pos, placer, itemstack, pointed_thing)
-	local meta = minetest.get_meta(pos)
-	local stack_meta = itemstack:get_meta()
-	local ferment = get_or_create_ferment(itemstack,stack_meta)
-	if ferment >0 then
-		meta:set_int("ferment", ferment)
-	end
-end
-
-local on_construct_ferment = function(pos)
-  --duration of ferment
-  local meta = minetest.get_meta(pos)
-  meta:set_int("ferment", get_or_create_ferment(pos))
-  --ferment
-  minetest.get_node_timer(pos):start(ferment_interval)
-end
-
--- custom function that preserves metadata from a replaced node to an itemstack
-local preserve_metadata_ferment = function(pos, oldnode, oldmeta, transferred_stack)
-  local imeta = transferred_stack:get_meta()
-  imeta:set_int("ferment",get_or_create_ferment(transferred_stack,oldmeta))
-end
-
-local on_timer_ferment = function(pos, elapsed)
-  local ferment_data = get_ferment_data(pos)
-  if not ferment_data then return false end
-  local can_ferment = true
-  local temp_range = ferment_data.temp_range
-  if temp_range then
-    --ferment if at right temp
-    local temp = climate.get_point_temp(pos)
-    if temp >= temp_range.min and temp <= temp_range.max then
-      can_ferment = true
-    else
-      can_ferment = false
-    end
-  end
-  if not can_ferment then return true end
-  -- only access meta if can ferment
-  local meta = minetest.get_meta(pos)
-  local ferment = meta:get_int("ferment")
-  -- catchup included
-  ferment = ferment - (elapsed >= (ferment_interval*2) and math.floor(elapsed/ferment_interval) or 1)
-  if ferment <= 1 then -- prevent possibility of refreshed fermenting at 0
-    minetest.swap_node(pos, {name = ferment_data.to})
-    return false
-  else
-    meta:set_int("ferment",ferment)
-  end
-  return true
-end
-
 -- Pot of new Tang (unfermented), must be left to ferment
 liquid_store.register_stored_liquid("tech:tang_unfermented",{
   source = "tech:unfermented_tang_liquid",
@@ -278,19 +192,19 @@ liquid_store.register_stored_liquid("tech:tang_unfermented",{
   _ferment_temp_range = {min=10,max=34},
   _ferment_to = "tech:tang",
   on_construct = function(pos)
-		on_construct_ferment(pos)
+		ncrafting.ferment_on_construct(pos)
 	end,
 	after_place_node = function(pos, placer, itemstack, pointed_thing)
-		after_place_ferment(pos, placer, itemstack, pointed_thing)
+		ncrafting.ferment_after_place(pos, placer, itemstack, pointed_thing)
 	end,
 	on_timer = function(pos, elapsed)
-		return on_timer_ferment(pos, elapsed)
+		return ncrafting.ferment_on_timer(pos, elapsed)
 	end,
   preserve_metadata = function(pos, oldnode, oldmeta, drops)
-    preserve_metadata_ferment(pos, oldnode, minetest.get_meta(pos), drops[1])
+    ncrafting.ferment_preserve_metadata(pos, oldnode, minetest.get_meta(pos), drops[1])
   end,
   _preserve_metadata = function(...) -- for liquid store interactions
-    preserve_metadata_ferment(...)
+    ncrafting.ferment_preserve_metadata(...)
   end,
 })
 -- wooden pot of unfermented tang
@@ -321,19 +235,19 @@ liquid_store.register_stored_liquid("tech:wooden_tang_unfermented",{
   _ferment_temp_range = {min=10,max=34},
   _ferment_to = "tech:wooden_tang",
   on_construct = function(pos)
-		on_construct_ferment(pos)
+		ncrafting.ferment_on_construct(pos)
 	end,
 	after_place_node = function(pos, placer, itemstack, pointed_thing)
-		after_place_ferment(pos, placer, itemstack, pointed_thing)
+		ncrafting.ferment_after_place(pos, placer, itemstack, pointed_thing)
 	end,
 	on_timer = function(pos, elapsed)
-		return on_timer_ferment(pos,elapsed)
+		return ncrafting.ferment_on_timer(pos, elapsed)
 	end,
   preserve_metadata = function(pos, oldnode, oldmeta, drops)
-    preserve_metadata_ferment(pos, oldnode, minetest.get_meta(pos), drops[1])
+    ncrafting.ferment_preserve_metadata(pos, oldnode, minetest.get_meta(pos), drops[1])
   end,
   _preserve_metadata = function(...) -- for liquid store interactions
-    preserve_metadata_ferment(...)
+    ncrafting.ferment_preserve_metadata(...)
   end,
 })
 
