5c3bf06425bb43813110b9b16b71a26991ce6a2a
TPH <DamotrixRob@gmail.com>
2024-07-06 02:17:48 -0400

    Backpacks can no longer dump or pack interact with protected storage or any storage node with the group "no_packdump"

--- mods/backpacks/init.lua
+++ mods/backpacks/init.lua
@@ -372,8 +372,13 @@ function backpacks.register_backpack(name, def)
   end
   def._on_use_item = function(player, itemstack, pointed_thing)
     if not (pointed_thing and pointed_thing.under) then return end
-    if not minimal.in_group(pointed_thing.under,"storage") then return end
-    show_packdump_formspec(pointed_thing.under, player, itemstack, def.can_dump, def.can_pack)
+    local pos = pointed_thing.under
+    local node = minetest.get_node(pos)
+    if not minimal.in_group(node,"storage") then return end
+    if minimal.in_group(node,"no_packdump") then return end
+    local pname = player:get_player_name()
+    if minetest.is_protected(pos,pname) then return end
+    show_packdump_formspec(pos, player, itemstack, def.can_dump, def.can_pack)
   end
   -- register backpack through storage.register_storage()
   storage.register_storage(":backpacks:backpack_"..name,def)
