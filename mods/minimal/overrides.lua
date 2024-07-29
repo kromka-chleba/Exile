275269298ee9d561f5b923d454b39defae7ff2f4
TPH <DamotrixRob@gmail.com>
2024-07-29 17:43:06 -0400

    Soft revamp of minetest.is_protected override, additional notation. minetest.is_protected now accepts meta as third parameter (to skip getting meta again). access_list is only checked if not the provided name/player is not the owner. "name" parameter can now be anything that has a "get_player_name" function

--- mods/minimal/overrides.lua
+++ mods/minimal/overrides.lua
@@ -36,35 +36,41 @@ end
 
 --Basic protection support
 local old_is_protected = minetest.is_protected
-function minetest.is_protected(pos, name)
-  if minetest.is_player(name) then
-    name = name:get_player_name()
-  end
+function minetest.is_protected(pos, name, pos_meta)
+  -- custom userdata or tables with get_player_name() permitted
+  name = type(name) == "string" and name or (type(name) == "userdata" or type(name) == "table")
+    and name.get_player_name and name:get_player_name()
   if type(name) ~= "string" then
     -- nil things can't touch stuff
     return true
   end
-   local pos_meta = minetest.get_meta(pos)
-   local owner = pos_meta:get_string("owner")
-   local bypass = minetest.check_player_privs(name, "protection_bypass")
-   -- Check if we have an access_list and if this player is on it
-   local access = false -- assume no access
-   local access_list
-   local list = pos_meta:get_string("access_list")
-   if list and list ~= "" then
-     access_list = minetest.parse_json(list)
-	 for _,granted in ipairs(access_list) do
-	   if name == granted then
-		 access = true
-		 break
-	   end
-	 end
-   end
+  -- you can specify the meta as third argument
+  pos_meta = type(pos_meta) == "userdata" and pos_meta or minetest.get_meta(pos)
+  local owner = pos_meta:get_string("owner") -- original protector of node
+  local bypass = minetest.check_player_privs(name, "protection_bypass")
+  -- check if owner is nil, owner is equal to name, or bypass, otherwise assume no access (false)
+  local access = ( owner == "" or owner == name or bypass ) or false
+  -- not the owner, check if there's an access_list and if they're on the VIP list
+  if not access then
+    local access_list = pos_meta:get_string("access_list")
+    -- if access_list, then parse its json
+    access_list = access_list ~= "" and minetest.parse_json(access_list) or nil
+    if access_list then
+      for _,granted in ipairs(access_list) do
+        if name == granted then
+          access = true
+          break
+        end
+      end
+    end
+  end
 
-   if not ( owner == "" or owner == name or bypass or access ) then
-      return true
-   end
-   return old_is_protected(pos, name)
+  -- no access, is protected
+  if not access then
+    return true
+  end
+  -- minetest's old protection always returns false so this is pointless...
+  return old_is_protected(pos, name, pos_meta)
 end
 
 local old_node_dig = minetest.node_dig
