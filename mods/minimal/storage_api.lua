82d601ae8a0782cfd2136473aacb2c0bb47237f5
TPH <DamotrixRob@gmail.com>
2024-08-17 05:14:15 -0400

    Systematizes "storage watchers" mechanism

--- mods/minimal/storage_api.lua
+++ mods/minimal/storage_api.lua
@@ -5,41 +5,9 @@ local modname = "minimal/storage_api.lua"
 local S = minimal.S
 
 -- functionality for determining if a player is looking in storage
-local storage_watched = {}
-local function get_watchers(pos, create)
-  pos = type(pos) == "string" and pos or type(pos) == "table" and minetest.pos_to_string(pos)
-  -- create a new watcher table if it doesn't exist
-  if create then
-    storage_watched[pos] = storage_watched[pos] or {}
-  end
-  return storage_watched[pos] or {} -- send watcher table or create an empty one
-end
-local function add_watcher(pos, pname)
-  local wt = get_watchers(pos, true) -- watch_table
-  pname = type(pname) == "string" and pname or (type(pname) == "userdata" or type(pname) == "table") and pname.get_player_name and pname:get_player_name()
-  -- iterate over table for nil indexes because Lua is dysfunctional when it comes to counting
-  for wi=1,(#wt + 1) do
-    if wt[wi] == pname then return false end
-    if wt[wi] == nil then
-      wt[wi] = pname
-      break
-    end
-  end
-end
-local function remove_watcher(pos, pname)
-  local wt = get_watchers(pos) -- watch_table
-  if #wt < 1 then return end -- no watch table
-  pname = type(pname) == "string" and pname or (type(pname) == "userdata" or type(pname) == "table") and pname.get_player_name and pname:get_player_name()
-  -- iterate over table to find name and remove
-  for wi=1,#wt do
-    if wt[wi] == pname then
-      wt[wi] = nil
-      -- delete table if empty
-      if #wt < 1 then storage_watched[pos] = nil end
-      return true
-    end
-  end
-end
+local get_watchers = minimal.get_watchers
+local add_watcher = minimal.add_watcher
+local remove_watcher = minimal.remove_watcher
 
 function storage.get_storage_formspec(pos, w, h, meta)
 	local creator = meta:get_string('creator')
---
+++ mods/minimal/storage_watcher_api.lua
@@ -0,0 +1,60 @@
+minimal = minimal
+local S = minimal.S
+
+-- functionality for determining if a player is looking in storage
+-- lets you set up proper functionality for when a player is or isn't looking at something, and committing to events accordingly
+-- e.g. playing sounds when a player looks into something or when they close out
+local storage_watched = {} -- table for determining watchers at a position
+
+-- get watchers
+-- get a table of watchers at a position, or an empty table if there isn't
+-- "create" is for creating a new watcher table to be used and is assigned to storage_watched
+-- shouldn't be used unless you're adding a watcher to the table ; used by add_watcher
+function minimal.get_watchers(pos, create)
+  -- pos can be a convertable position or any string of your liking
+  pos = type(pos) == "string" and pos or type(pos) == "table" and minetest.pos_to_string(pos)
+  -- create a new watcher table if it doesn't exist
+  if create then
+    storage_watched[pos] = storage_watched[pos] or {}
+  end
+  return storage_watched[pos] or {} -- send watcher table or create an empty one
+end
+
+-- add a watcher
+-- adds a player watching storage to the provided pos (or any form of "string" name technically)
+function minimal.add_watcher(pos, pname)
+  local wt = minimal.get_watchers(pos, true) -- watch_table (2nd boolean will create a new table to add to if there isn't one)
+  pname = type(pname) == "string" and pname or (type(pname) == "userdata" or type(pname) == "table") and pname.get_player_name and pname:get_player_name()
+  -- iterate over table for nil indexes because Lua is dysfunctional when it comes to counting
+  -- iterate +1 to get a nil index at the end to add (in case there isn't a random nil index)
+  for wi=1,(#wt + 1) do
+    -- if we find ourselves then return false for failure
+    if wt[wi] == pname then return false end
+    -- if we find a nil index, add to
+    if wt[wi] == nil then
+      wt[wi] = pname
+      return true
+    end
+  end
+  -- due to check for self and check for nil being in the same for loop, it is possible on an off chance for a watcher to
+  -- be added twice, Lua is weird!
+  -- won't matter beyond getting a genuine count, as remove_watcher will iterate through the entire table to remove all instances
+end
+
+-- remove a watcher
+-- removes a player watching storage from a specified pos (if it exists and has more than 0 watchers)
+-- deletes table if emptied
+function minimal.remove_watcher(pos, pname)
+  local wt = minimal.get_watchers(pos) -- watch_table
+  if #wt < 1 then return end -- no watch table
+  pname = type(pname) == "string" and pname or (type(pname) == "userdata" or type(pname) == "table") and pname.get_player_name and pname:get_player_name()
+  -- iterate over table to find name and remove
+  for wi=1,#wt do
+    if wt[wi] == pname then
+      wt[wi] = nil
+      -- delete table if empty
+      if #wt < 1 then storage_watched[pos] = nil end
+      return true
+    end
+  end
+end
