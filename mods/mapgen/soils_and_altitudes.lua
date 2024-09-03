d219cd601f94e0d63dff209cb05fb5d504912aad
Dokimi <22285919+DokimiCU@users.noreply.github.com>
2024-09-03 14:03:09 +1200

    register coquina for mapgen

--- mods/mapgen/soils_and_altitudes.lua
+++ mods/mapgen/soils_and_altitudes.lua
@@ -134,19 +134,19 @@ return {
     },
 
     gravel_on = {
-        "nodes_nature:granite", "nodes_nature:limestone", "nodes_nature:gneiss", "nodes_nature:conglomerate",
+        "nodes_nature:granite", "nodes_nature:limestone", "nodes_nature:coquina", "nodes_nature:gneiss", "nodes_nature:conglomerate",
     },
 
     sand_on = {
-        "nodes_nature:granite", "nodes_nature:limestone", "nodes_nature:gneiss", "nodes_nature:sandstone",
+        "nodes_nature:granite", "nodes_nature:limestone", "nodes_nature:coquina", "nodes_nature:gneiss", "nodes_nature:sandstone",
     },
 
     silt_on = {
-        "nodes_nature:granite", "nodes_nature:limestone", "nodes_nature:gneiss", "nodes_nature:siltstone",
+        "nodes_nature:granite", "nodes_nature:limestone", "nodes_nature:coquina", "nodes_nature:gneiss", "nodes_nature:siltstone",
     },
 
     clay_on = {
-        "nodes_nature:granite", "nodes_nature:limestone", "nodes_nature:gneiss", "nodes_nature:claystone",
+        "nodes_nature:granite", "nodes_nature:limestone", "nodes_nature:coquina", "nodes_nature:gneiss", "nodes_nature:claystone",
     },
 
     cave_mushrooms_on = {
@@ -159,8 +159,8 @@ return {
     },
 
     cave_egg_on = {
-        "nodes_nature:granite", "nodes_nature:limestone", "nodes_nature:ironstone", "nodes_nature:gneiss",
-        "nodes_nature:granite_boulder", "nodes_nature:limestone_boulder", "nodes_nature:ironstone_boulder", "nodes_nature:gneiss_boulder",
+        "nodes_nature:granite", "nodes_nature:limestone", "nodes_nature:coquina", "nodes_nature:ironstone", "nodes_nature:gneiss",
+        "nodes_nature:granite_boulder", "nodes_nature:limestone_boulder", "nodes_nature:coquina_boulder", "nodes_nature:ironstone_boulder", "nodes_nature:gneiss_boulder",
         "nodes_nature:sandstone", "nodes_nature:siltstone", "nodes_nature:claystone", "nodes_nature:conglomerate",
     },
 }
