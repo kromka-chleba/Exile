0cfe61dcdcf3d8cef03aa1a8ec03a4cdd490a100
Jeremy <jeremylshannon@gmail.com>
2024-09-04 20:47:25 -0700

    Format mods/rings

--- mods/rings/init.lua
+++ mods/rings/init.lua
@@ -29,26 +29,26 @@ dofile(minetest.get_modpath("rings") .. "/noise.lua")
 noise_handler = noise_handler
 
 minetest.register_node("rings:antiquorium", {
-	description = artifacts_S("Antiquorium"),
-	tiles = {"artifacts_antiquorium.png"},
-	stack_max = minimal.stack_max_bulky *4,
-	sounds = nodes_nature.node_sound_glass_defaults(),
-	paramtype = "light",
-	groups = {cracky = 1, not_in_creative_inventory = 1},
-	drop = "artifacts:antiquorium"
+                          description = artifacts_S("Antiquorium"),
+                          tiles = {"artifacts_antiquorium.png"},
+                          stack_max = minimal.stack_max_bulky *4,
+                          sounds = nodes_nature.node_sound_glass_defaults(),
+                          paramtype = "light",
+                          groups = {cracky = 1, not_in_creative_inventory = 1},
+                          drop = "artifacts:antiquorium"
 })
 minetest.register_node("rings:moon_glass", {
-	description = artifacts_S("Moon Glass"),
-	drawtype = "glasslike",
-	tiles = {"artifacts_moon_glass.png"},
-	stack_max = minimal.stack_max_bulky *4,
-	light_source = 5,
-	paramtype = "light",
-	sunlight_propagates  = true,
-	use_texture_alpha = c_alpha.clip,
-	sounds = nodes_nature.node_sound_glass_defaults(),
-	groups = {cracky = 1, not_in_creative_inventory = 1},
-	drop = "artifacts:moon_glass",
+                          description = artifacts_S("Moon Glass"),
+                          drawtype = "glasslike",
+                          tiles = {"artifacts_moon_glass.png"},
+                          stack_max = minimal.stack_max_bulky *4,
+                          light_source = 5,
+                          paramtype = "light",
+                          sunlight_propagates  = true,
+                          use_texture_alpha = c_alpha.clip,
+                          sounds = nodes_nature.node_sound_glass_defaults(),
+                          groups = {cracky = 1, not_in_creative_inventory = 1},
+                          drop = "artifacts:moon_glass",
 })
 
 
@@ -64,50 +64,50 @@ local c_ring_i = minetest.get_content_id("rings:moon_glass")
 
 local c_node_list = {}
 minetest.register_on_mods_loaded(function()
-	-- get content ids for all tree types
-	local tree_list = {}
-	tree_list.maraka  = minetest.get_content_id("nodes_nature:maraka_tree" )
-	tree_list.tangkal = minetest.get_content_id("nodes_nature:tangkal_tree")
-	tree_list.sasaran = minetest.get_content_id("nodes_nature:sasaran_tree")
-	tree_list.kagum   = minetest.get_content_id("nodes_nature:kagum_tree" )
-	tree_list.panasee = minetest.get_content_id("nodes_nature:panasee_tree" )
-	tree_list.amma    = minetest.get_content_id("nodes_nature:amma_tree" )
-	tree_list.daoja   = minetest.get_content_id("nodes_nature:daoja_tree" )
+      -- get content ids for all tree types
+      local tree_list = {}
+      tree_list.maraka  = minetest.get_content_id("nodes_nature:maraka_tree" )
+      tree_list.tangkal = minetest.get_content_id("nodes_nature:tangkal_tree")
+      tree_list.sasaran = minetest.get_content_id("nodes_nature:sasaran_tree")
+      tree_list.kagum   = minetest.get_content_id("nodes_nature:kagum_tree" )
+      tree_list.panasee = minetest.get_content_id("nodes_nature:panasee_tree" )
+      tree_list.amma    = minetest.get_content_id("nodes_nature:amma_tree" )
+      tree_list.daoja   = minetest.get_content_id("nodes_nature:daoja_tree" )
 
-	for _ , v in pairs(tree_list) do
-		c_node_list[v] = true
-	end
+      for _ , v in pairs(tree_list) do
+         c_node_list[v] = true
+      end
 
-	-- debug info.  uncomment here and set exile_debug to true to enable
-	--exile.debug.log_to_world( "c_ring_o = " .. c_ring_o .. "\n" )
-	--exile.debug.log_to_world( "c_ring_i = " .. c_ring_i .. "\n" )
-	--exile.debug.log_to_world( "tree_list = " .. dump(tree_list) .. "\n" )
-	--exile.debug.log_to_world( "c_node_list = " .. dump(c_node_list) .. "\n\n" )
+      -- debug info.  uncomment here and set exile_debug to true to enable
+      --exile.debug.log_to_world( "c_ring_o = " .. c_ring_o .. "\n" )
+      --exile.debug.log_to_world( "c_ring_i = " .. c_ring_i .. "\n" )
+      --exile.debug.log_to_world( "tree_list = " .. dump(tree_list) .. "\n" )
+      --exile.debug.log_to_world( "c_node_list = " .. dump(c_node_list) .. "\n\n" )
 end)
 
 
 --noise parameters
 -- base terrain height
 np_2d_base = {
-    offset = 0,
-    scale = 7,
-    spread = {x = 80, y = 80, z = 80},
-    seed = 0,
-    octaves = 3,
-    persist = 0.5,
-    lacunarity = 3,
-	-- flags = "noeased",
+   offset = 0,
+   scale = 7,
+   spread = {x = 80, y = 80, z = 80},
+   seed = 0,
+   octaves = 3,
+   persist = 0.5,
+   lacunarity = 3,
+   -- flags = "noeased",
 }
 -- cracks in the shells of the great rings
 np_3d_ring = {
-    offset = 0,
-    scale = 1,
-    spread = {x = 8, y = 8, z = 8},
-    seed = 0,
-    octaves = 1,
-    persist = 1,
-    lacunarity = 1.0,
-    -- flags = "absvalue",
+   offset = 0,
+   scale = 1,
+   spread = {x = 8, y = 8, z = 8},
+   seed = 0,
+   octaves = 1,
+   persist = 1,
+   lacunarity = 1.0,
+   -- flags = "absvalue",
 }
 
 local chunk_size = {x = 80, y = 80, z = 80}
@@ -119,92 +119,92 @@ local data = {}
 
 
 minetest.register_on_generated(function(minp, maxp, blockseed)
-    -- check block elevation (not too high or low)
-    if maxp.y < 40 or minp.y > 80 then
-		return
-	end
+      -- check block elevation (not too high or low)
+      if maxp.y < 40 or minp.y > 80 then
+         return
+      end
 
-	-- 1-in-70 chance of generating a ring
-    if math.floor(blockseed / 100) % 70 > 0 then
-	return
-    end
+      -- 1-in-70 chance of generating a ring
+      if math.floor(blockseed / 100) % 70 > 0 then
+         return
+      end
 
-    -- get voxelmanip node data
-    local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
-    local area = VoxelArea:new({MinEdge=emin, MaxEdge=emax})
-    vm:get_data(data)
+      -- get voxelmanip node data
+      local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
+      local area = VoxelArea:new({MinEdge=emin, MaxEdge=emax})
+      vm:get_data(data)
 
-    -- noise & randomness
-    local nvals_2d_b = nobj_2d_b:get_2d_map_flat(minp)
-    local nvals_3d_r = nobj_3d_r:get_3d_map_flat(minp)
+      -- noise & randomness
+      local nvals_2d_b = nobj_2d_b:get_2d_map_flat(minp)
+      local nvals_3d_r = nobj_3d_r:get_3d_map_flat(minp)
 
-    math.randomseed(blockseed)
+      math.randomseed(blockseed)
 
-    -- ring axis direction and centroid position
-    -- axis vector is random inside a 1.0 x 0.5 x 1.0 box, centered on (0,0,0)
-    -- centroid is horizontally centered in chunk, elevation based on 2d noise
-    local rdir, rpos
-    rdir = vector.new(math.random() - 0.5,
-		      (math.random() - 0.5)/2, math.random() - 0.5)
-    rpos = {}
-    rpos.x = math.floor((minp.x + maxp.x) / 2)
-    rpos.z = math.floor((minp.z + maxp.z) / 2)
-    rpos.y = math.floor(nvals_2d_b[(rpos.z-minp.z) * 80 + (rpos.x-minp.x) + 1])
+      -- ring axis direction and centroid position
+      -- axis vector is random inside a 1.0 x 0.5 x 1.0 box, centered on (0,0,0)
+      -- centroid is horizontally centered in chunk, elevation based on 2d noise
+      local rdir, rpos
+      rdir = vector.new(math.random() - 0.5,
+                        (math.random() - 0.5)/2, math.random() - 0.5)
+      rpos = {}
+      rpos.x = math.floor((minp.x + maxp.x) / 2)
+      rpos.z = math.floor((minp.z + maxp.z) / 2)
+      rpos.y = math.floor(nvals_2d_b[(rpos.z-minp.z) * 80 + (rpos.x-minp.x) + 1])
 
-    -- debug info.  uncomment here and set exile_debug to true to enable
-    --exile.debug.log_to_world( "\nblockseed = " .. blockseed .. "\n" )
-    --exile.debug.log_to_world( "emin=" .. dump(emin) .. ")\n" )
-    --exile.debug.log_to_world( "emax=" .. dump(emax) .. ")\n" )
-    --exile.debug.log_to_world( "rpos=(" .. rpos.x .. ',' .. rpos.y .. ',' .. rpos.z .. ")\n" )
-    --exile.debug.log_to_world( "rdir=(" .. rdir.x .. ',' .. rdir.y .. ',' .. rdir.z .. ")\n" )
-    --exile.debug.log_to_world( "rdir_length=" .. vector.length(rdir) .. "\n" )
+      -- debug info.  uncomment here and set exile_debug to true to enable
+      --exile.debug.log_to_world( "\nblockseed = " .. blockseed .. "\n" )
+      --exile.debug.log_to_world( "emin=" .. dump(emin) .. ")\n" )
+      --exile.debug.log_to_world( "emax=" .. dump(emax) .. ")\n" )
+      --exile.debug.log_to_world( "rpos=(" .. rpos.x .. ',' .. rpos.y .. ',' .. rpos.z .. ")\n" )
+      --exile.debug.log_to_world( "rdir=(" .. rdir.x .. ',' .. rdir.y .. ',' .. rdir.z .. ")\n" )
+      --exile.debug.log_to_world( "rdir_length=" .. vector.length(rdir) .. "\n" )
 
-    -- shells are defined by node distance from the ring centroid
-    -- use distance squared to avoid computationally expensive square root operations
-    local dd_o_min = math.pow(30, 2) -- outer shell min distance squared
-    local dd_i_min = math.pow(31, 2) -- inner shell min distance squared
-    local dd_i_max = math.pow(34, 2) -- inner shell max distance squared
-    local dd_o_max = math.pow(35, 2) -- outer shell max distance squared
+      -- shells are defined by node distance from the ring centroid
+      -- use distance squared to avoid computationally expensive square root operations
+      local dd_o_min = math.pow(30, 2) -- outer shell min distance squared
+      local dd_i_min = math.pow(31, 2) -- inner shell min distance squared
+      local dd_i_max = math.pow(34, 2) -- inner shell max distance squared
+      local dd_o_max = math.pow(35, 2) -- outer shell max distance squared
 
-    for z = minp.z, maxp.z do
-       for y = minp.y, maxp.y do
-	  for x = minp.x, maxp.x do
+      for z = minp.z, maxp.z do
+         for y = minp.y, maxp.y do
+            for x = minp.x, maxp.x do
 
-	     local vi = area:index(x, y, z)
+               local vi = area:index(x, y, z)
 
-	     if not c_node_list[data[vi]] then -- not at a tree node
+               if not c_node_list[data[vi]] then -- not at a tree node
 
-		local dd_node = math.pow(rpos.x - x, 2)
-		   + math.pow(rpos.z - z, 2)
-		   + math.pow(rpos.y - y, 2)	-- node distance squared
-		local s_proj = math.abs((x-rpos.x)
-		   * rdir.x + (y-rpos.y)
-		   * rdir.y + (z-rpos.z) * rdir.z)
-		-- node dist projected on axis and scaled by axis length
-		local nv_3d_r = nvals_3d_r[(z - minp.z) * 80 * 80
-		   + (y - minp.y) * 80 + (x - minp.x) + 1]
-		-- outer shell crack noise
+                  local dd_node = math.pow(rpos.x - x, 2)
+                     + math.pow(rpos.z - z, 2)
+                     + math.pow(rpos.y - y, 2)	-- node distance squared
+                  local s_proj = math.abs((x-rpos.x)
+                     * rdir.x + (y-rpos.y)
+                     * rdir.y + (z-rpos.z) * rdir.z)
+                  -- node dist projected on axis and scaled by axis length
+                  local nv_3d_r = nvals_3d_r[(z - minp.z) * 80 * 80
+                     + (y - minp.y) * 80 + (x - minp.x) + 1]
+                  -- outer shell crack noise
 
-		if (dd_node > dd_i_min) and (dd_node < dd_i_max)
-		   and (s_proj <= 2.0) then
-		   data[vi] = c_ring_i  -- place inner shell
-		end
+                  if (dd_node > dd_i_min) and (dd_node < dd_i_max)
+                     and (s_proj <= 2.0) then
+                     data[vi] = c_ring_i  -- place inner shell
+                  end
 
-		if (dd_node > dd_o_min) and (dd_node < dd_o_max)
-		   and (s_proj <= 3.0) and (nv_3d_r < 0.3)
-		   and (data[vi] ~= c_ring_i) then
-		   data[vi] = c_ring_o  -- place outer shell
-		end
+                  if (dd_node > dd_o_min) and (dd_node < dd_o_max)
+                     and (s_proj <= 3.0) and (nv_3d_r < 0.3)
+                     and (data[vi] ~= c_ring_i) then
+                     data[vi] = c_ring_o  -- place outer shell
+                  end
 
-	     end
+               end
 
-	  end
-       end
-    end
+            end
+         end
+      end
 
-    vm:set_data(data)
-    vm:calc_lighting()
-    vm:write_to_map()
-    minetest.fix_light(minp, maxp)
+      vm:set_data(data)
+      vm:calc_lighting()
+      vm:write_to_map()
+      minetest.fix_light(minp, maxp)
 
 end)
