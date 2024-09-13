d99527e1b1475ea7d40347bcc4140513b7c5aff5
lili36 <lili3600@hotmail.fr>
2024-09-13 17:26:00 +0200

    code cleaning : number of line and size of grid passed as local variables for more clarity

--- mods/minimal/interface/inventory.lua
+++ mods/minimal/interface/inventory.lua
@@ -384,6 +384,10 @@ end
 -- or selected inventory changes
 -- It is triggered by setting cache.recipesFS = nil
 local function cache_player_recipes(cache, player_name, pInv)
+    -- this is for more clarity, choice of display settings
+    local line_number = 3 -- 3 lines of recipes displayed
+    local grid_size = 1.2 -- size of a square of recipe : 1*1 of image + 0.1 margins around, used to place them on a grid, including tabs
+    --
     local recipesFS = {}
     local sItem = cache.sItem    -- craft type Item selected
     local sTab = cache.sTab              -- selected craft type tab
@@ -455,7 +459,7 @@ local function cache_player_recipes(cache, player_name, pInv)
     -- Add tab header
     recipesFS[#recipesFS + 1] = "style_type[item_image_button;border=false]"
     for i=1, #cTabs do
-        local leftPoint = 3.3 + (i - 1) * 1.2
+        local leftPoint = 3.3 + (i - 1) * grid_size
         local item_name = crafting.icon_item_name[cTabs[i]]
             or 'crafting:placeholder'
         recipesFS[#recipesFS + 1] = 'item_image_button['..(leftPoint)..
@@ -467,18 +471,19 @@ local function cache_player_recipes(cache, player_name, pInv)
     end
     -- add Scrollable container
     local columns = 6 -- can show 6 items accross without scrollbar
-    if #recipe_list > 18 then
-        columns = 5
-        local scroll_max = math.ceil(#recipe_list / 4 )
+    if #recipe_list > columns * line_number then
+        columns = columns -1 -- discard a line to make room for scrollbar
+        local scroll_max = math.ceil(#recipe_list / columns)-line_number 
         recipesFS[#recipesFS + 1] =
             'scrollbaroptions[max=' .. tonumber(scroll_max) .. ';'
-            .. 'smallstep=1;largestep=1;thumbsize=1]'
+            .. 'smallstep=1;largestep=line_number;thumbsize=1]'
         recipesFS[#recipesFS + 1]
             = 'scrollbar[9.2,1.2;.5,3.45;vertical;recipes_scroll;'
             .. sScroll .. ']'
     end
     recipesFS[#recipesFS + 1] = 'scroll_container[3.2,1;'..
-        tostring(columns + 1)..',3.75;recipes_scroll;vertical;1.25]'
+        tostring(columns + 1)..',3.75;recipes_scroll;vertical;' .. 
+        grid_size .. ']'
     -- Add recipe buttons in columns of 5 or 6
     local x = 0
     local y = 0
@@ -488,7 +493,7 @@ local function cache_player_recipes(cache, player_name, pInv)
         local item_description=ItemStack(recipe_output):get_description()
 
         local id = result.recipe.id
-        local bg_coords =  tostring(x * 1.2) ..','.. tostring(y * 1.2 + 0.2)
+        local bg_coords =  tostring(x * grid_size) ..','.. tostring(y * grid_size + 0.2)
         -- set background image
         local bg_image
         local craftable = result.craftable
@@ -501,7 +506,8 @@ local function cache_player_recipes(cache, player_name, pInv)
             ";1,1;" .. bg_image .. "]"
         -- Add button image
         local btn_coords =
-            tostring( x* 1.2 + 0.1 ) .. ','..tostring( y * 1.2 + 0.3 )
+            tostring( x * grid_size + 0.1 ) .. ','..
+            tostring( y * grid_size + 0.3 )
         recipesFS[#recipesFS + 1] = 'item_image_button['
             .. btn_coords .. ';.8,.8;'
             .. recipe_output .. ';sResult_' .. id ..';]'
