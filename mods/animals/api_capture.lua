a7b218a947d1bb5cf9546a6a4d058b86a1a126ce
TPH <DamotrixRob@gmail.com>
2024-09-19 22:57:16 -0400

    Fixes doubled tooltips by removing a setting of description in the animals.capture function

--- mods/animals/api_capture.lua
+++ mods/animals/api_capture.lua
@@ -290,11 +290,6 @@ animals.capture = function(self, clicker)
             stack_meta:set_string(key, minetest.serialize(value))
         end
     end
-    local idef = minetest.registered_items[self.name] or {}
-    if idef._tool_tips and idef._tool_tips ~= '' then
-        stack_meta:set_string('description',
-                              idef.description .. idef._tool_tips)
-    end
 
     local inv = clicker:get_inventory()
     if inv:room_for_item("main", new_stack) then
