d76734103a3d60b266459d87c2dcee9f728e5275
Jeremy <jeremylshannon@gmail.com>
2024-09-22 13:22:22 -0700

    interface/inventory: support image_button in addition to item_image_button

--- mods/minimal/interface/inventory.lua
+++ mods/minimal/interface/inventory.lua
@@ -512,8 +512,15 @@ local function cache_player_recipes(cache, player_name, pInv)
         local leftPoint = 3.3 + (i - 1) * grid_size
         local item_name = crafting.icon_item_name[cTabs[i]]
             or 'crafting:placeholder'
-        recipesFS[#recipesFS + 1] = 'item_image_button['..(leftPoint)..
-            ',0.3;0.6,0.6;'.. item_name .. ';sCraftTab_'..i..';]'
+        local button_type = 'item_image_button['
+        local suffix = ']'
+        if item_name == string.gsub(item_name, ":", "") then -- not an item
+            button_type = 'image_button['
+            -- item_image_button and image_button formats differ, so..
+            suffix = ';;false]' -- hide borders on item_, which has extra fields
+        end
+        recipesFS[#recipesFS + 1] = button_type..(leftPoint)..
+            ',0.3;0.6,0.6;'.. item_name .. ';sCraftTab_'..i..';'..suffix
         recipesFS[#recipesFS + 1] = 'tooltip[sCraftTab_'.. i ..
             ';' .. minetest.formspec_escape((crafting.tab_labels[cTabs[i]]
                                              or cTabs[i])) ..
