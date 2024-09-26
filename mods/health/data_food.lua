37b866db1c35ce2389e5ef244876a8d278dff54a
TPH <DamotrixRob@gmail.com>
2024-09-26 18:55:55 -0400

    Wintery rhuya flour can now be cooked, burns to regular rhuya flour

--- mods/health/data_food.lua
+++ mods/health/data_food.lua
@@ -161,7 +161,8 @@ HEALTH.bake_table = {
     ["animals:carcass_fish_small"] = {temp = 100, time = 6},
     ["animals:carcass_fish_large"] = {temp = 100, time = 18},
     ['tech:yolk_and_albumen'] = {temp = 100, time = 2},
-    ['tech:rhuya_flour'] = {temp = 100, time = 5}
+    ['tech:rhuya_flour'] = {temp = 100, time = 5},
+    ['tech:rhuya_flour_wintery'] = {temp = 100, time = 8, burned='tech:rhuya_flour_burned'}
 }
 
 -- tags (what diseases to spawn, single string or table of diseases), ch=chance, sv=severity
