ddbc2bdc639b9d5a3cfa311c989c9e76ce5bf399
Jeremy <jeremylshannon@gmail.com>
2024-10-02 12:04:19 -0700

    Reduce the size of antiquorium button borders

--- mods/minimal/interface/themes.lua
+++ mods/minimal/interface/themes.lua
@@ -4,12 +4,12 @@ local themes = {
     ["Antiglass"] =
         "bgcolor[#080808BB;false]"..
         "background9[0,0;,;9slice-hollow.png;true;10]"..
-        "style_type[button;bgimg=9slice.png;bgimg_middle=10]"..
+        "style_type[button;bgimg=9slice.png;bgimg_middle=6]"..
         "listcolors[#00000069;#5A5A5A;#141318;#30434C;#FFF]",
     ["Antiblue"] =
         "bgcolor[#000008BB;true]"..
         "background9[0,0;,;9slice-deep.png;true;10]"..
-        "style_type[button;bgimg=9slice.png;bgimg_middle=10]"..
+        "style_type[button;bgimg=9slice.png;bgimg_middle=6]"..
         "listcolors[#00000069;#5A5A5A;#141318;#30434C;#FFF]",
     ["Legacy Grey"] =
         "bgcolor[#080808BB;true]"..
