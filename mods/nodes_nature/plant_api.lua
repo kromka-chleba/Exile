3ad46e0bd58f54eecf05fc3fb0af578ca5ca75d8
TPH <DamotrixRob@gmail.com>
2024-10-07 04:48:59 -0400

    Simplify seed props generation (less lines), add more notation for it (and correct one part)

--- mods/nodes_nature/plant_api.lua
+++ mods/nodes_nature/plant_api.lua
@@ -813,25 +813,24 @@ end
 
 function plant.get_seed_base_props(plant_def)
     local next_life_stage = plant.get_seedling_name(plant_def.name, 1)
-    local seed_texture, seed_description, inventory_seed_image
+    -- spores or seeds (default to seeds)
+    local seed_type = (plant_def.lifeform_type == "mushroom" or plant_def.plant_type == "moss") and 
+        "spores" or "seeds"
+    -- choose between spores texture or seeds texture
+    local seed_texture = seed_type == "spores" and "nodes_nature_spores.png" or
+        "nodes_nature_seeds.png"
+    -- choose spore description if spores, otherwise default to seeds description
+    local seed_description = seed_type == "spores" and S("@1 Spores", plant_def.description) or
+        S("@1 Seeds", plant_def.description)
     -- plant image to be shown in left corner of seeds
-    -- get dead fruiting texture if only_deaf_fruit, get fruiting texture if fruiting, or
+    -- get dead fruiting texture if only_dead_fruit, get fruiting texture if fruiting, or
     -- otherwise use regular plant texture
     local plant_img = plant_def.only_dead_fruit and plant.get_dead_texture_name(plant_def.name) or
         plant_def.fruit and plant.get_fruiting_texture_name(plant_def.name) or 
         plant.get_texture_name(plant_def.name)
-    if plant_def.lifeform_type == "mushroom" or
-        plant_def.plant_type == "moss" then
-        seed_texture = "nodes_nature_spores.png"
-        inventory_seed_image = "([combine:32x32:0,0="..
-            plant_img..")^[resize:16x16^nodes_nature_spores.png"
-        seed_description = S("@1 Spores", plant_def.description)
-    else
-        seed_texture = "nodes_nature_seeds.png"
-        inventory_seed_image = "([combine:32x32:0,0="..
-            plant_img..")^[resize:16x16^nodes_nature_seeds.png"
-        seed_description = S("@1 Seeds", plant_def.description)
-    end
+    -- generate inventory_image for seed using plant_img crop and seed_texture
+    local inventory_seed_image = "([combine:32x32:0,0="..
+        plant_img..")^[resize:16x16^"..seed_texture
     local props = {
         description = plant_def.seed_description or seed_description,
         tiles = {seed_texture},
