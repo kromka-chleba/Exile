fdc6f440f59011094f3667f822c84db4b00c8a53
Jeremy <jeremylshannon@gmail.com>
2024-10-25 13:00:41 -0700

    Alias v4's sasaran_fruit back to sasaran_cone for v3, fixes #999

--- mods/minimal/aliases.lua
+++ mods/minimal/aliases.lua
@@ -5,38 +5,42 @@
 --Changed in Exile 0.3.5c/0.2.8c
 --Swapping illumination out for wielded_light
 minetest.register_alias("illumination:light_faint",
-			"air")
+                        "air")
 minetest.register_alias("illumination:light_dim",
-			"air")
+                        "air")
 minetest.register_alias("illumination:light_mid",
-			"air")
+                        "air")
 minetest.register_alias("illumination:light_full",
-			"air")
+                        "air")
 
 --Changed in Exile 0.3.5/0.2.8
 --standardizing uncooked/cooked/burned
 minetest.register_alias("tech:anperla_tuber_peeled",
-			"tech:peeled_anperla")
+                        "tech:peeled_anperla")
 minetest.register_alias("tech:anperla_tuber_cooked",
-			"tech:peeled_anperla_cooked")
+                        "tech:peeled_anperla_cooked")
 minetest.register_alias("tech:maraka_cake_unbaked",
-			"tech:maraka_bread")
+                        "tech:maraka_bread")
 minetest.register_alias("tech:maraka_cake",
-			"tech:maraka_bread_cooked")
+                        "tech:maraka_bread_cooked")
 minetest.register_alias("tech:maraka_cake_burned",
-			"tech:maraka_bread_burned")
+                        "tech:maraka_bread_burned")
 
 --Changed in Exile 0.3.1/0.2.4
 minetest.register_alias("tech:peeled_anperal_tuber",
-			"tech:anperla_tuber_peeled")
+                        "tech:anperla_tuber_peeled")
 minetest.register_alias("tech:cooked_anperal_tuber",
-			"tech:anperla_tuber_cooked")
+                        "tech:anperla_tuber_cooked")
 minetest.register_alias("tech:mashed_anperal",
-			"tech:mashed_anperla")
+                        "tech:mashed_anperla")
 minetest.register_alias("tech:mashed_anperal_cooked",
-			"tech:mashed_anperla_cooked")
+                        "tech:mashed_anperla_cooked")
 minetest.register_alias("tech:mashed_anperal_burned",
-			"tech:mashed_anperla_burned")
+                        "tech:mashed_anperla_burned")
 
 minetest.register_alias("tech:clay_oil_lamp_empty",
-			"tech:clay_oil_lamp_unlit")
+                        "tech:clay_oil_lamp_unlit")
+
+-- Accidentally backported from v4
+minetest.register_alias("nodes_nature:sasaran_fruit",
+                        "nodes_nature:sasaran_cone")
