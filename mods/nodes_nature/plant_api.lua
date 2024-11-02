990c19fd58a7856da6f22cd1ab1bfa1fd237b08e
TPH <DamotrixRob@gmail.com>
2024-11-01 22:10:38 -0400

    Reintroduce need to specify dye_candidate

--- mods/nodes_nature/plant_api.lua
+++ mods/nodes_nature/plant_api.lua
@@ -156,10 +156,7 @@ function plant.new(def)
         or {-0.4, -0.5, -0.4, 0.4, -0.2, 0.4}
     def.seedling_nodebox = def.seedling_nodebox
         or {-0.2, -0.5, -0.2, 0.2, -0.3, 0.2}
-    -- if we have a dominant color specified, then we're a dye_candidate!
-    -- at least, if dye_candidate isn't set as false
-    def.dye_candidate = type(def.dye_candidate) ~= "boolean" and (def.dominant_color and true) or
-        def.dye_candidate or false
+    def.dye_candidate = def.dye_candidate or false
     -- fruit mechanics
     -- set winter_fruit to true if only_dead_fruit is provided
     def.winter_fruit = def.winter_fruit or def.only_dead_fruit and true or false
