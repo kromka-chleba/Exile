e8486c9c9f2a465d51aef0ee4bf0c2d1d1f90ad5
Somedumbguy <john@sed.ninja>
2024-11-18 15:49:43 -0500

    fix spacing

--- mods/nodes_nature/compost.lua
+++ mods/nodes_nature/compost.lua
@@ -94,8 +94,8 @@ local function decompose_compost(pos, elapsed, dc_name)
     else
         meta:set_int("decomposition", decomposition)
 	-- compost generates a bit of heat while it's breaking down
-	local temp_effect = speed == wet_speed and 10 or 5
-	climate.air_temp_source(vector.new(pos.x, pos.y+1, pos.z), temp_effect, 40, 0.75, decomposition_interval / 4)
+        local temp_effect = speed == wet_speed and 10 or 5
+        climate.air_temp_source(vector.new(pos.x, pos.y+1, pos.z), temp_effect, 40, 0.75, decomposition_interval / 4)
         return true
     end
 end
