13e0dfe2b9573108a62db46e7e6dcff62176a98b
TPH <DamotrixRob@gmail.com>
2024-12-16 22:54:35 +0000

    Adds "wet" option for ncrafting.fertilize, code improvements for ncrafting/nature.lua Fixes #1062 (#1063)
    As the title says :D
    
    Fixes the issue with wet  compost being used twice
    
    Reviewed-on: https://codeberg.org/Mantar/Exile/pulls/1063
    Co-authored-by: TPH <DamotrixRob@gmail.com>
    Co-committed-by: TPH <DamotrixRob@gmail.com>

--- mods/ncrafting/nature.lua
+++ mods/ncrafting/nature.lua
@@ -1,67 +1,59 @@
 ncrafting = ncrafting
 
-local function get_soil_pos(pos)
-    local node = minetest.get_node(pos)
-    if (minimal.is_group(node.name,"flora")
-        or minimal.is_group(node.name,"seed")) then
-        
+-- figure out if a soil - gets soil underneath plants
+local function get_soil_pos(pos, ndef)
+    ndef = ndef or minimal.get_nodedef(pos)
+    local groups = ndef and ndef.groups
+    if not groups then return end
+    -- if plant or seed, check underneath
+    if groups.flora or groups.seed then
         pos.y = pos.y - 1
-        node = minetest.get_node(pos)
+        ndef = minimal.get_nodedef(pos)
+        groups = ndef and ndef.groups
+        if not groups then return end
     end
-    if minimal.is_group(node.name,"sediment") or
-        minimal.is_group(node.name,"dry_sediment")  then
-        return pos
-        -- permit interactions with compost
-    elseif minetest.get_item_group(node.name,"compost") == 1
-        or minetest.get_item_group(node.name,"undecomposed_compost") == 1 then
-
-        return pos
+    -- now to check if a sediment
+    if groups.sediment or groups.dry_sediment or
+      -- permit interactions with compost
+      groups.compost or groups.undecomposed_compost then
+        -- return pos and nodedef
+        return pos, ndef
     end
-    return
 end
 
 -- provides "wet" as a suffix already, so that all that needs to be applied
 -- is either nothing or "salty" to get "wet_salty"
 -- may remove this in the future if different liquids exist
-local function wet_soil(pos, suffix)
-    if not vector.check(pos) then
-        return
-    end
+local function wet_soil(pos, ndef, suffix)
+    if not vector.check(pos) then return end
     local node = minetest.get_node(pos)
-    if (type(suffix) ~= "string") then
-        suffix = ""
-    else
-        -- lowercase it :D
-        suffix = string.lower(suffix)
-    end
-    if (suffix ~= "" and string.sub(suffix,1,1) ~= "_") then
-        -- add underscore if not found
-        suffix = "_"..suffix
-    end
-    if (suffix == "_wet") then
-        -- we define it later on, clear it
-        suffix = ""
-    end
-    suffix = "_wet"..suffix
+    ndef = ndef or minetest.registered_nodes[node.name]
+    ndef = ndef or minimal.get_nodedef(ndef)
+    -- set suffix as empty or lowercase if provided
+    suffix = type(suffix) ~= "string" and "" or suffix:lower()
+    -- we define _wet later on, clear it - otherwise continue as normal
+    suffix = (suffix == "wet" or suffix == "_wet") and "" or suffix
+    -- add "_wet" prefix
+    -- if not empty, add an underscore if not provided, otherwise add prefix to suffix normally
+    suffix = suffix ~= "" and (suffix:sub(1,1) ~= "_" and "_wet_"..suffix or "_wet"..suffix) or "_wet"
 
     -- check if watered block exists
     local wet_node_name = node.name .. suffix
     -- otherwise do normal checking
-    if not minetest.registered_nodes[wet_node_name] and
-        string.match(node.name,"depleted") then
+    if not minetest.registered_nodes[wet_node_name] and node.name:match("depleted") then
         -- depleted nodes with roots have "depleted" behind the "roots", so
         --  I declare this if statement first
         -- IF the provided node is "depleted", look for its proper depleted
         --  wet variant
+        wet_node_name = wet_node_name:gsub("_depleted","")
         wet_node_name = string.gsub(wet_node_name,"_depleted","")
         wet_node_name = wet_node_name.."_depleted"
         -- we didn't erase "_wet" from the name
     end
-    if not minetest.registered_nodes[wet_node_name]
-        and string.match(node.name,"roots") then
+    if not minetest.registered_nodes[wet_node_name] and node.name:match("roots") then
         -- IF the provided node is "roots", look for its proper roots
         --  wet variant
-        wet_node_name = string.gsub(wet_node_name,"_roots","")
+        wet_node_name = wet_node_name:gsub("_roots","")
         wet_node_name = wet_node_name.."_roots"
         -- we didn't erase "_wet" from the name
     end
@@ -83,7 +75,7 @@ function ncrafting.water_soil(itemstack, user, pointed_thing, node_suffix, empty
     assert(type(pointed_thing) == "table",
            "ncrafting.water_soil: provided pointed_thing is not a table! "..
            "got: "..type(pointed_thing))
-    if not empty_container then
+    if itemstack and not empty_container then
         local storeddef = liquid_store.get_sl_def(itemstack:get_name())
         empty_container = storeddef and storeddef.nodename_empty
     end
@@ -92,19 +84,33 @@ function ncrafting.water_soil(itemstack, user, pointed_thing, node_suffix, empty
            type(empty_container))
 
     -- can only water nodes
-    if (pointed_thing.type == "node"
-        and itemstack) then
-        local pos = get_soil_pos(pointed_thing.under)
+    if pointed_thing.type == "node"
+        and itemstack then
+        local pos, ndef = get_soil_pos(pointed_thing.under)
         -- will only return a pos if a soil is found
-        if (vector.check(pos) and wet_soil(pos, node_suffix)) then
+        if pos and wet_soil(pos, ndef, node_suffix) then
             -- if position is good and can wet the soil
             -- check if player is in creative
             if minimal.player_in_creative(user) then
                 return
             end
             -- if not in creative, empty the can out
-            itemstack:take_item()
-            return ItemStack(empty_container), true
+            empty_container = ItemStack(empty_container)
+            -- huh, how do you have more than one watering? ok then...
+            if itemstack:get_count() > 1 then
+                itemstack:take_item() -- take 1 from slot
+                local inv = core.is_player(user) and user:get_inventory()
+                if inv:room_for_item("main", empty_container) then
+                    inv:add_item("main", empty_container)
+                else
+                    minimal.warn_inv_full(user)
+                    core.add_item(user:get_pos(), empty_container)
+                end
+            -- not more than 1, replace
+            else
+                itemstack = empty_container
+            end
+            return itemstack, true
         end
     end
 
@@ -114,16 +120,15 @@ function ncrafting.water_soil(itemstack, user, pointed_thing, node_suffix, empty
 end
 
 -- fertilize soil with a fertilizer
-function ncrafting.fertilize(pos, puncher, itemstack)
+-- 4th parameter boolean "wet" used to wet the soil on success
+function ncrafting.fertilize(pos, puncher, itemstack, wet)
     assert(vector.check(pos),
            "ncrafting.fertilize: provided position is not a position!")
     local inv
     local replace_with = ""
     if minetest.is_player(puncher) then
         inv = puncher:get_inventory()
-        if not itemstack then
-            itemstack = puncher:get_wielded_item()
-        end
+        itemstack = itemstack or puncher:get_wielded_item()
     end
     if not itemstack or itemstack:get_name() == "" then
         -- no itemstack, or have a hand? NO FERTILIZATION!
@@ -131,21 +136,19 @@ function ncrafting.fertilize(pos, puncher, itemstack)
     end
 
     pos = get_soil_pos(pos)
-    if not pos then
-        return
-    end
+    if not pos then return end
     local ndef = minimal.get_nodedef(pos)
-    local itemdef = minetest.registered_nodes[itemstack:get_name()]
-    if not (ndef and itemdef) then
-        -- definition don't exist
-        return
-    end
+    local itemdef = itemstack:get_definition()
+    -- no definition!
+    if not (ndef and itemdef) then return end
+    -- no groups??
+    if not itemdef.groups then return end
 
     -- find all possible variations of a replaceable
     if type(itemdef._fertilize_replace_with) == "string" then
         replace_with = itemdef._fertilize_replace_with
     else
-        local slab = string.gsub(itemstack:get_name(),ndef.mod_origin..":","")
+        local slab = itemdef.name:gsub(ndef.mod_origin..":","")
         slab = "stairs:slab_"..slab
         if minetest.registered_nodes[slab] then
             replace_with = slab
@@ -155,11 +158,13 @@ function ncrafting.fertilize(pos, puncher, itemstack)
     replace_with = ItemStack(replace_with)
 
     -- avoid having to set up a "fertilized" boolean to prevent another check over
-    local function complete()
-
+    local function complete(node_name)
         -- allow a custom "after_fertilize" function
         if type(itemdef._after_fertilize) == "function" then
             return itemdef._after_fertilize(pos, itemstack, puncher)
+        end
+        if wet and node_name then
+            wet_soil(pos, core.registered_nodes[node_name])
         end
         -- proceed as usual
         if not replace_with then
@@ -168,17 +173,18 @@ function ncrafting.fertilize(pos, puncher, itemstack)
         end
         itemstack:take_item()
         if inv then
-            if itemstack:get_count() <= 0 then
+            if itemstack:is_empty() then
                 inv:remove_item("main",itemstack)
             end
             if inv:room_for_item("main",replace_with) then
                 inv:add_item("main",replace_with)
             else
+                minimal.warn_inv_full(puncher)
                 minetest.item_drop(replace_with, puncher, pos)
             end
         else
             -- no inventory found
-            if (itemstack:get_count() > 0) then
+            if itemstack:get_count() > 0 then
                 itemstack:take_item()
             end
             -- no inventory to place into
@@ -188,39 +194,36 @@ function ncrafting.fertilize(pos, puncher, itemstack)
         -- give a third parameter telling the function that it went well
     end
 
-    -- find fertilizing/composting
+    -- prioritize fertilizing/composting first
     local node_name = ndef._fertile_name
     if node_name == ndef.name then
         -- prevent the ability to fertilize something already fertilized lol
         return itemstack
     end
-    if (minimal.is_group(itemdef.name,"compost")
-        and type(node_name) == "string") then
-
+    if itemdef.groups.compost and type(node_name) == "string" then
         if not minetest.registered_nodes[node_name] then
-            minetest.log("error","ncrafting: "..node_name..
+            minetest.log("error","ncrafting.fertilize: "..node_name..
                          " is not a valid item/node!")
             return itemstack
         end
         minetest.swap_node(pos, {name = node_name})
-        return complete()
+        return complete(node_name)
     end
 
-    -- find enriching
+    -- prioritize enriching second
     node_name = ndef._rich_name
     if node_name == ndef.name then
         -- prevent the ability to enrich something already enriched lol
         return itemstack
     end
-    if ( minimal.is_group(itemdef.name,"fertilizer" )
-         and type(node_name) == "string" ) then
+    if itemdef.groups.fertilizer and type(node_name) == "string" then
         if not minetest.registered_nodes[node_name] then
-            minetest.log("error","ncrafting: '"..node_name..
+            minetest.log("error","ncrafting.fertilize: '"..node_name..
                          "' is not a valid item/node!")
             return itemstack
         end
         minetest.swap_node(pos, {name = node_name})
-        return complete()
+        return complete(node_name)
     end
 
     return itemstack
