8a439262b563d50b4f4ca7d889ca599af07c672d
TPH <DamotrixRob@gmail.com>
2024-12-22 13:29:20 -0500

    Saltwater now plays a sound if boiled for salt

--- mods/tech/waterworking.lua
+++ mods/tech/waterworking.lua
@@ -133,7 +133,13 @@ liquid_store.register_stored_liquid(
             local name = minetest.get_node(pos).name
             -- temp of 102C
             -- change name from clay_water_salt_water to clay_water_pot_dry_salt
-            return ncrafting.roast(pos, name, name:sub(1,#name-10).."pot_dry_salt", nil, 102)
+            if not ncrafting.roast(pos, name, name:sub(1,#name-10).."pot_dry_salt", nil, 102) then
+                -- complete, play boil sound
+                minimal.sound_play({name = "tech_boil", pos = pos, max_hear_distance = 10,
+                    gain = 0.8, pitch = {0.95,1.2}})
+                return false
+            end
+            return true
         end
 })
 
