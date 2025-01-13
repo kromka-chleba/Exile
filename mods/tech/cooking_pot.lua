f60009119a23ab4c26c9c5fae6dcfc7f2beb31da
TPH <DamotrixRob@gmail.com>
2025-01-13 06:32:36 -0500

    tech/cooking_pot: uh... actually let's have that edible ~= 1 check be a edible == 2 check becuase we can't put cookable inedible to edible stuff in pots otherwise!

--- mods/tech/cooking_pot.lua
+++ mods/tech/cooking_pot.lua
@@ -998,9 +998,9 @@ minetest.register_node("tech:cooking_pot",{
     on_receive_fields = pot_receive_fields,
     allow_metadata_inventory_put = function(pos, listname, index, stack, player)
         local stackdef = stack:get_definition()
-        -- can't be added to soup (not edible == 1 or is no_soup)
+        -- can't be added to soup (edible == 2 or is no_soup)
         if stackdef.groups and
-            (stackdef.groups.no_soup and stackdef.groups.no_soup > 0 or stackdef.groups.edible ~= 1) then
+            (stackdef.groups.no_soup and stackdef.groups.no_soup > 0 or stackdef.groups.edible == 2) then
             return 0
         end
         -- not a valid food or cookable
