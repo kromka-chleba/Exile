8c61a66581c20f2d102072def62fd2af901104be
TPH <DamotrixRob@gmail.com>
2025-02-24 17:26:23 -0500

    nodes_nature/liquids: fix crash with cracky detection for lava eruptions... ugh - TPH

--- mods/nodes_nature/liquids.lua
+++ mods/nodes_nature/liquids.lua
@@ -577,7 +577,7 @@ local erupt_melt = function(name, ch, dh)
     -- use cracky group to determine how to melt down (chances lower as you go down)
     -- use current height divided by desired height to determine "strength", the lower the stronger
     -- rig the cracky game to prevent easy basalt melting lol
-    cracky = cracky < 3 and cracky * 0.05
+    cracky = cracky < 3 and cracky * 0.05 or cracky
     cracky = (cracky/3)*(1-(ch/dh))
     if ran()<cracky then return true end
 end
