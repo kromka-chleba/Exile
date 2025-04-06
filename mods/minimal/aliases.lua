a37abcc654254f0c24f176c374d755da3cbd2d9a
Dokimi <22285919+DokimiCU@users.noreply.github.com>
2025-04-07 11:23:19 +1200

    add alias for damo

--- mods/minimal/aliases.lua
+++ mods/minimal/aliases.lua
@@ -50,6 +50,8 @@ minetest.register_alias("tech:broken_pottery_block",
 --Changed in v4
 minetest.register_alias("tech:paint_scratching",
                         "tech:stone_etcher")
+minetest.register_alias("nodes_nature:damo",
+                        "nodes_nature:damo_fruitless")
 --Changes to v4 during v4 dev
 -- prior rzepicha root craftitems --> actual plant registration
 minetest.register_alias_force("nodes_nature:rzepicha_root","nodes_nature:rzepicha_fruitless")
