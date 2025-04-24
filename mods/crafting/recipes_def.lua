71634e17e71d19f5707c4a5c453c38ad4fbfec5c
lili36 <nomail>
2025-04-24 10:49:46 +0200

    CRAFTING mod: refactoring step2: crafting types and recipes tables
    * creation of local table for crafting types (instead of one global table for label, one for icon and one for sound)
    + various access functions, not all used yet.
    + localise recipe per id and adds crafting.get_recipe() to get that list (only used in tests)
    + deletion of recipes per output table
    + moving crafting table from api.lua to init.lua
    + update of README.md for craft types and recipes
    + adjustement so current mods using crafting.recipes table can still work.

--- mods/crafting/recipes_def.lua
+++ mods/crafting/recipes_def.lua
@@ -1,16 +1,119 @@
 --CRAFT TYPES and RECIPES registrations and calls ------------------------------
---------------------------------------------------------------------------------
 
+-- Crafting types --------------------------------------------------------------
+
+--[[table of crafting types with follwing format :
+    * [name] as index
+    * following fields :
+        * `label`     - label to display, is `name` if label not provided
+        * `icon`      - icon to disply in tab
+        * `recipes`   - table of possible recipes for that crafting type
+        * `sound`
+    ]]
+local craft_types = {}
+
+-- only here for tests/api_spec.lua
+function crafting.reset_craft_types()
+    craft_types = {}
+end
+
+function crafting.is_type_registered(name)
+    if not craft_types[name] then
+        return false
+    else
+        return true
+    end
+end
+
+-- get type table for that `name`
+function crafting.get_type(name)
+    return craft_types[name]
+end
+
+-- get recipes table for that crafting type
+function crafting.get_recipes_by_type(type)
+    local tab = craft_types[type]
+    if not tab then
+        core.log (type .. " is not a valid registered type, returning nil")
+        return nil
+    else
+        return craft_types[type].recipes
+    end
+end
+
+-- gives a list of all registered crafting types' names
+function crafting.get_registered_types_names()
+    local t= {}
+    for name, _ in pairs(craft_types) do
+        t[#t+1] = name
+    end
+    return t
+end
+
+-- can go when exile_meals mod adapted
+crafting.recipes = {}
+
+--[[register craft types (used for stations and tabs):
+    *name: the name of the type
+    *label: what will be displayed in game (tab) and translated
+    *icon_item_name: for image tab
+    *sound: sound made when we craft a recipe of that type
+]]
 function crafting.register_type(name, label, icon_item_name, sound)
-    crafting.recipes[name] = {}
+    if name == nil then
+        core.log ("missing 'name' in crafting.register_type.\nRegistration cancelled")
+        return
+    end
+    -- if type is already registered, warn the user
+    -- #TODO make a function to modify it ??
+    if craft_types[name] then
+        core.log("warning", "type "
+        .. name
+        .. "is already registered. Use crafting.get_registered_types() to get the registered list.\n"
+        .. "type will be overriden")
+    end
+
+    local new_type = {}
+    new_type.name = name
     -- add a label for tabs - default to the name
-    crafting.tab_labels[name] = (label or name)
-    crafting.icon_item_name[name] = icon_item_name
+    new_type.label = (label or name)
+    new_type.icon = icon_item_name
+    new_type.recipes = {}
+
     -- set up sound mechanism
-    sound = type(sound) == "string" and {name = sound} or type(sound) == "table" and sound
+    if type(sound) == "string" then
+        sound = {name = sound}
+    elseif sound and type(sound) ~= "table" then
+        core.log ("wrong 'sound' type in crafting.register_type of " .. name)
+    end
     if sound and type(sound.name) == "string" then
         sound.max_hear_distance = sound.max_hear_distance or 10
-        crafting.sounds[name] = sound
+        new_type.sound = sound
+    end
+
+    craft_types[name] = new_type
+
+    -- can go when exile_meals mod adapted
+    crafting.recipes[name] = crafting.get_recipes_by_type(name)
+end
+
+-- Recipes ---------------------------------------------------------
+
+--array of all registered recipes, by `id`
+local recipes_by_id = {}
+
+--#TODO still used ?
+function crafting.get_recipes()
+    return recipes_by_id
+end
+
+-- get recipe by ID
+function crafting.get_recipe(id)
+    if type(id) == "number" then
+        return recipes_by_id[id]
+    else
+        core.log("wrong param type in crafting.get_recipe, number expected, got" .. type(id))
+        return
     end
 end
 
@@ -73,9 +176,9 @@ function crafting.register_recipe(def)
     -- custom preview for formspec
     def._display = def._display
 
-    def.id = #crafting.recipes_by_id + 1
-    crafting.recipes_by_output[def.output] = def
-    crafting.recipes_by_id[def.id] = def
+    def.id = #recipes_by_id + 1
+
+    recipes_by_id[def.id] = def
     return def.id
 end
 
@@ -128,22 +231,20 @@ local function check_recipe_def(def)
     return true -- recipe is ok
 end
 
--- have to wait for all modules load before generating
--- station lists
+-- have to wait for all modules load before generating list fo recipes per crafting type
 minetest.register_on_mods_loaded( function ()
-        for _,recipe in ipairs(crafting.recipes_by_id) do
+        for _,recipe in ipairs(recipes_by_id) do
             check_recipe_def(recipe)
             if type(recipe.type) == "string" then
                 recipe.type = { recipe.type }
             end
             for _,station in ipairs(recipe.type) do
-                local tab = crafting.recipes[station]
-                assert(tab,        "Unknown craft type " .. station)
-                tab[#tab + 1] = recipe
+                local t = craft_types[station]
+                if not t then
+                    core.log("Unknown craft type: " .. station)
+                else
+                    table.insert(t.recipes, recipe)
+                end
             end
         end
 end)
-
-function crafting.get_recipe(id)
-    return crafting.recipes_by_id[id]
-end
