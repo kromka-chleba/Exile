87a53bc23b33641e3cc53cab24c612a5c59fd0d0
lili36 <nomail>
2025-06-07 13:50:16 +0200

    Deduplication of recipes, using new replace crafting system + liquid_store replace function
    food_craft.lua : deduplication of doughs recipes
    drugs.lua : refactoring using new craft replacement system to deduplicate tang and vinegar recipes

--- mods/tech/food_crafts.lua
+++ mods/tech/food_crafts.lua
@@ -906,18 +906,23 @@ for dough,flour in pairs(
         count = 8
         flour = flour.." "..count
     end
-    -- iterate through every empty water pot
-    for _,water_pot in pairs({"tech:clay_water_pot", "tech:wooden_water_pot"}) do
-        crafting.register_recipe({
-            type = "breadmaking",
-            output = dough.." "..count,
-            -- only the freshwater variant
-            items = {flour, water_pot.."_freshwater"},
-            replace = water_pot,
-            level = 1,
-            always_known = true
-        })
-    end
+
+    local recipe_def = {
+        type = "breadmaking",
+        output = dough.." "..count,
+        -- only the freshwater variant
+        items = {flour, "group:freshwater/pot"},
+        -- replace with empty pot
+        replace = function(item_name)
+            -- replace freshwater container by correct empty version
+            if core.get_item_group(item_name, "freshwater") > 0 then
+                return liquid_store.replace(item_name, nil)
+            end
+        end,
+        level = 1,
+        always_known = true
+    }
+    crafting.register_recipe(recipe_def)
 end
 
 -- ANIMAL PRODUCTS
