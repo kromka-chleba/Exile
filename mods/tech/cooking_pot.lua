9233338495cc99b285be78ed2a1de51bca61e473
lili36 <nomail>
2025-06-14 03:52:01 +0200

    register group description for water groups, pots and bowls
    addition of bowl and pot translation in tech, to be used in group 
    descriptions
    
    adding "bowl" group to bowls, and "empty" group to empty one in 
    registration, to be used with the new crafting group system.

--- mods/tech/cooking_pot.lua
+++ mods/tech/cooking_pot.lua
@@ -283,6 +283,10 @@ local function register_food_bowl_filled(name, def, empty, food_table, transfer,
     def.groups.falling_node = def.groups.falling_node or 1
     def.groups.dig_immediate = def.groups.dig_immediate or 3
     def.groups.food_bowl_filled = 1
+    -- since groups are inherited from empty bowl,
+    -- I need to remove 'empty' group from filled bowl
+    def.groups.empty = nil
+
     -- whether or not we should have functions relating to metadata
     -- dependent upon groups for alternative "true" - edible 2 will make it true if not provided
     save_meta = type(save_meta) ~= "boolean" and def.groups.edible == 2 or type(save_meta) == "boolean" and save_meta
@@ -340,6 +344,7 @@ local function register_food_bowl_filled(name, def, empty, food_table, transfer,
             def.groups[ind] = nil
         end
     end
+
     -- register transfer functions + functionality
     if transfer then
         -- set soup functions
@@ -481,10 +486,17 @@ local function register_food_bowl(name, def)
     end
     -- final touches to the empty bowl
     def.on_soup_transfer = def.on_soup_transfer or soup_transfer
+    -- crafting groups
+    def.groups.bowl = 1 -- for recipes
+    def.groups.empty = 1 -- needs to be removed in filled bowl
+    -- registration
     minetest.register_node(name,def)
 end
 -- namespace
 tech.register_food_bowl = register_food_bowl
+
+-- adding custom group description to be used in crafting recipes
+crafting.register_group_desc("bowl", S("Bowl"))
 
 -- registration of clay + wooden food bowls, and their soup + stew variants
 register_food_bowl("food_bowl_clay")
