9abd50e6afcde15847087b1087738db2d9d729b7
lili36 <nomail>
2025-06-20 21:18:04 +0200

    fix for the hand (issue #1272).

--- mods/player_api/hand.lua
+++ mods/player_api/hand.lua
@@ -1,12 +1,6 @@
 -- we have to do this because damage groups does not properly transfer for held items
 -- with a custom hand, because yay! Engine!!!
-core.register_item(":",{
-    type = "none",
-    tool_capabilities = {
-        damage_groups = {fleshy=minimal.hand_dmg},
-    },
-    groups = {not_in_creative_inventory = 1}
-})
+core.override_item("", {tool_capabilities = {damage_groups = {fleshy=minimal.hand_dmg}}, liquids_pointable = true})
 -- The hand
 -- does not override core.registered_items[""]
 -- use core.registered_items["player_api:hand"] instead
@@ -15,7 +9,6 @@ minetest.register_item(
         type = "none",
         wield_image = "wieldhand.png",
         wield_scale = {x=1,y=1,z=2.5},
-        liquids_pointable = true,
         tool_capabilities = {
             full_punch_interval = minimal.hand_punch_int,
             max_drop_level = minimal.hand_max_lvl,
@@ -32,8 +25,8 @@ minetest.register_item(
                         [2]=minimal.hand_crum*minimal.t_scale2,
                         [3]=minimal.hand_crum}, uses=0
                 },
-                damage_groups = {fleshy=minimal.hand_dmg}
             },
+            damage_groups = {fleshy=minimal.hand_dmg}
         },
         groups = {not_in_creative_inventory = 1}
 })
