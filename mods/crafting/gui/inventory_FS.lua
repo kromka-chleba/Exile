19a13e7967059982466953ed05d863ac469a182c
Algor3 <Algor3@noreply.codeberg.org>
2025-07-30 23:34:41 +0200

    Cleanup: Remove code for the 'Crafting' page
    Removes
    - Code of hidden 'Crafing' page from inventory,
    - hack with context.open_inv which was used to tell the page's get() function
      if it's in inventory or not, i.e. in a station,
    - commented code for 'Open recipes' button and for processing of its event,
    - cache.to_updated - used to control the commented recipe button,
    - the now unused function set_next_sfinv_page() that was required to decide
      whether a different page needs to be shown instead of crafting when inventory
      is opened next time (becuase otherwise it would not be up to date
    
    Replaces:
    - screenshot of crafting in Inventory for the README.md

--- mods/crafting/gui/inventory_FS.lua
+++ mods/crafting/gui/inventory_FS.lua
@@ -2,52 +2,9 @@ local S = minetest.get_translator("crafting")
 -- Crafting formspec in inventory formspec -------------------------------------
 --------------------------------------------------------------------------------
 
--- Register crafting formspec as inv tab
+-- Register help formspec as inv tab
 if not minetest.global_exists("sfinv") then error("Sfinv is missing?") end
 
--- I don't use overrider to have crafting page AFTER creative tbas
--- TODO better way to deal with tab order to implement
-sfinv.register_page(
-    "crafting:crafting", {
-        title = S("Crafting"),
-        get = function(self, player, context)
-            -- initiates FS_cache[player_name] if non existant
-            local cache = crafting.get_FS_cache(player, true)
-
-            -- flag formspec as open if we changed tab in sfinv
-            if context.open_inv then
-                cache = crafting.open_formspec(player, "", cache)
-            end
-
-            -- last parameter is to indicate if inv fs is open for sure
-            local formspec = crafting.make_crafting_formspec(player, cache)
-            return sfinv.make_formspec_for_exile(player, context,
-                                                 formspec, false)
-        end,
-        on_player_receive_fields = function(self, player, context, fields)
-            -- if something changed, redraw the page
-            local cache = crafting.process_receive_fields(player, "", fields)
-            if cache then
-                sfinv.set_player_inventory_formspec(player, context)
-            end
-            return true -- stop checking other events
-        end,
-        -- selecting the tab from an other tab or seting to crafting tab
-        on_enter = function(self, player, context)
-            -- WARNING: is called even if sfinv is closed, this is why I don't update the cache as "open" here
-            -- reset station if needed (for next sfinv opening)
-            crafting.set_station(player, nil)
-            -- it is called before `get` function so formspec will be generated AFTER on_enter call
-        end,
-        -- triggered when leaving tab or by calling sfinv.set_page
-        -- WARNING: be careful to not call sfinv.set_page here or you will get an infinite loop...
-        on_leave = function(self, player, context)
-            -- gives back input panel items and update player's cache
-            crafting.close_crafting_formspec(player)
-        end
-})
-
--- Register help formspec as inv tab
 sfinv.register_page(
     "crafting:help", {
         title = S("Crafting?"),
@@ -76,7 +33,3 @@ sfinv.register_page(
 -- (we explicitly set the default page to clothing:clothing
 -- in mods/player_api/init.lua after that page's per-player stuff is initialized).
 sfinv.override_page("sfinv:crafting", {is_in_nav=function(...) return false end})
-
--- for PR 1299 (-> no more crafting in inventory), but clean-up, i.e. removal
--- of the page's registration to sfinv postponed until a later commit
-sfinv.override_page("crafting:crafting", {is_in_nav=function(...) return false end})
