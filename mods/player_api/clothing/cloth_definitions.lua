49d67c07983a18092a487070a477a073d456548b
lili36 <lili36@noreply.codeberg.org>
2025-08-20 19:56:11 +0200

    moving on_secondary_use to _on_use_item to equip cloths from inventory with Aux1, and not with right click anymore
    renaming player_api.on_rightclick to player_api._on_use_item and adding comments to it
    
    removal of obsolete comment

--- mods/player_api/clothing/cloth_definitions.lua
+++ mods/player_api/clothing/cloth_definitions.lua
@@ -165,20 +165,14 @@ function player_api.register_cloth(name, def)
         palette = "natural_dyes.png",
         groups = def.groups or nil,
     }
+
     -- below functions will be added to blankets - but will not have any clothing functionality
-    newdef.on_secondary_use = function(itemstack, user, pointed_thing)
-        local under = pointed_thing.under
-        if under then
-            -- "under" will not exist if pointed_thing is not a node
-            local nodedef = minimal.get_nodedef(under)
-            if nodedef and nodedef.on_rightclick then
-                return nodedef.on_rightclick(under, minetest.get_node(under),
-                                             user, itemstack, pointed_thing)
-            end
-        end
-        return player_api.on_rightclick(itemstack, user, pointed_thing)
+
+    -- below function let us equip the cloth using action key (Aux1)
+    newdef._on_use_item = function(player, wielded_item, pointed_thing)
+        return player_api._on_use_item(wielded_item, player, pointed_thing)
     end
-    newdef.on_place = newdef.on_secondary_use -- no point for on_place, make it run on_secondary_use
+
     if def.customfields then
         for k, d in pairs(def.customfields) do
             newdef[k] = d
