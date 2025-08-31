620b7628fdf80da90ff44c4909e9c049c3eaace2
lili36 <lili36@noreply.codeberg.org>
2025-08-31 06:55:25 +0200

    2nd fix in bed_rest, bed_meta is called bmeta

--- mods/bed_rest/functions.lua
+++ mods/bed_rest/functions.lua
@@ -104,8 +104,8 @@ local function equip_blanket(player, bed_pos, bed_meta)
         p_inv:set_stack('blanket',1,blanket)
         bedInv:set_stack('main',1,ItemStack(''))
         -- blanket is on me (or no blanket) and not in the bed's inv anymore
-        bed_meta:set_string("blanket","")
-        minimal.infotext_set_new(bed_pos, bed_meta)
+        bmeta:set_string("blanket","")
+        minimal.infotext_set_new(bed_pos, bmeta)
 
     -- else, try to take one from inventory
     else
