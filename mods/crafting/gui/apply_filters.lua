f32adfdf92dc46312e409253bfda39b66a02ab3b
Algor3 <Algor3@noreply.codeberg.org>
2025-09-02 20:27:23 +0200

    Crafting: filter button with icons instead of checkbox with text
    + 'Save this': fxed position for the button so that it stays under the
      mouse pointer while the search bar moves up and down when the filter
      gets enabled or disabled
    + Have buttons for 'Use this' and ' Save this' positioned between the
      relevant input grid and the recipe panel - like with crafting buttons
    + add workflow indicator to the filter buttons
    + force formspec refresh when switching the state does only change the
      appearance of the filter button but not change visibilitiy of recipes
      That case is very rare. Usually there are changes so that an update was
      not suppressed. So the additional case does not matter regarding
      performance.

--- mods/crafting/gui/apply_filters.lua
+++ mods/crafting/gui/apply_filters.lua
@@ -77,15 +77,17 @@ local function apply_filters_to_list(cache, r_list)
     return it_changed
 end
 
--- apply filters to given recipe list and updated recipe panel
-local function cache_apply_filters(cache, optional_list)
+-- Apply filters to given recipe list and updated recipe panel.
+-- An actual update of the recipe panel is suppressed unless it would actually
+-- change or force_update is true.
+local function cache_apply_filters(cache, optional_list, force_update)
     if not optional_list then
         optional_list = cache.recipes
     end
 
     local it_changed = apply_filters_to_list(cache, optional_list)
 
-    if it_changed then
+    if it_changed or force_update then
         -- erases recipes panel formspec part
         cache.FS_recipes = nil
         -- reset scroll bar to top
