8ffcde238cdb921f99f74fa0114592e26245800f
lili36 <lili36@noreply.codeberg.org>
2025-10-07 13:15:00 +0200

    Adds a function in liquid_store to register containers nodes + change the name of the containers groups one

--- mods/tech/waterworking.lua
+++ mods/tech/waterworking.lua
@@ -189,7 +189,7 @@ for _, container in pairs ({"water_pot", "watering_can"}) do
             -- adds pot group for recipes
             def.groups.pot = 1
             -- adds group "pot" in container
-            liquid_store.register_container(name, {"pot"})
+            liquid_store.register_container_groups(name, {"pot"})
             -- #TODO maybe cleaner function to reunite th 2 lines above ?
         end
         minetest.register_node(name, def)
