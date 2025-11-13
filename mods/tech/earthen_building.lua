kromka-chleba/Exile

commit f55fff2be5727187667c6980986c7e8a51d7bd47

Copy all sediment groups to wicker well-lined sediment -- fixes #1441

mods/tech/earthen_building.lua
@@ -162,15 +162,22 @@ for i = 1, #soiltable do
     wdef._wet_salty_name = bn..soiltable[i]..wtable[3]
     for j = 1, #wtable do
         local wdef2 = table.copy(wdef)
+
+        -- Copy groups, minus falling_node
+        local sedname = "nodes_nature:"..soiltable[i]..wtable[j]
+        wdef2.groups = core.registered_nodes[sedname].groups
+        wdef2.groups["falling_node"] = nil
+
         wdef2.description = wwdesc[j]
-        wdef2.groups.wet_sediment = j - 1
-        if j == 1 then wdef2.groups.dry_sediment = 1 end
+
+        -- Arrange tiles
         local tile = "nodes_nature_"..soiltable[i]..".png"
         if j > 1 then tile = tile.."^nodes_nature_mud.png" end
         if j == 3 then tile = tile.."^nodes_nature_mud_salt.png" end
         local wicker = "tech_wattle.png"
         if j > 1 then wicker = wicker.."^nodes_nature_mud.png" end
         wdef2.tiles = {wicker, tile, tile, tile, tile, tile }
+
         minetest.register_node(bn..soiltable[i]..wtable[j],
                                wdef2)
     end

mods/nodes_nature/moisture_spread.lua
@@ -21,7 +21,8 @@ local function water_erode(pos, node)
     --take the sediment under it and move it to the side
     local pos_under = {x = pos.x, y = pos.y - 1, z = pos.z}
     local under_name = minetest.get_node(pos_under).name
-    if minetest.get_item_group(under_name, "sediment") > 0 then
+    if core.get_item_group(under_name, "sediment") > 0 and
+        core.get_item_group(under_name, "falling_node") > 0 then
 
         --move it to another part of water, so long as it is grounded
         local pos_flow = minetest.find_nodes_in_area(

commit c5e0caf87251fb15b9b149fc889a43594666dad0

make sediment fall again after being broken in f55fff2be5

mods/tech/earthen_building.lua
@@ -165,7 +165,7 @@ for i = 1, #soiltable do
 
         -- Copy groups, minus falling_node
         local sedname = "nodes_nature:"..soiltable[i]..wtable[j]
-        wdef2.groups = core.registered_nodes[sedname].groups
+        wdef2.groups = table.copy(core.registered_nodes[sedname].groups)
         wdef2.groups["falling_node"] = nil
 
         wdef2.description = wwdesc[j]