kromka-chleba/Exile

commit 8cf5c0afccb17fb54d376cf1ba485c34d56752ce

sfinv: fix crash when taking from creative inv after '/revokeme creative'
+ fix the wrong display of creative inventory after '/revokeme creative'
+ adds custom per player homepages

mods/sfinv/api.lua
@@ -36,9 +36,25 @@ sfinv = {
 }
 
 local homepage_name = "sfinv:crafting"
+-- per player homepages
+local homepage_names = {}
 
-function sfinv.set_homepage_name(new_name)
-	homepage_name = new_name
+-- Sets the name of the page to be set initially when a player joins as well as
+-- after set_context() was called with a nil context.
+-- `new_name`: name of the page to set for `player`
+-- WARNING: Make sure to not set a page as homepage before every prerequisites
+--          of that page are set up for `player`. E.g. if some player-specific
+--          pre-reqs are initialized in a callback registered with
+--          register_on_joinplayer() then you could set the page safely in the
+--          same function or any time later. Otherwise you might see crashes
+--          due to sfinv setting the page too early.
+function sfinv.set_homepage_name(player, new_name)
+    local name = player:get_player_name()
+    if name then
+        homepage_names[name] = new_name
+    else
+        homepage_names[name] = nil
+    end
 end
 
 -- default tab_order will be order of registration
@@ -170,7 +186,10 @@ end
 --------------------------------------------------------------------------------
 
 function sfinv.get_homepage_name(player)
-	return homepage_name
+    local page_name = homepage_names[player:get_player_name()]
+    if sfinv.pages[page_name] then return page_name end
+
+    return homepage_name
 end
 
 -- in order to be able to set custom tab order

mods/creative/init.lua
@@ -105,12 +105,11 @@ local function update_creative_attributes(plr, granter_name, onnew, nonotif)
         else
             return ccuf(name, nil) -- remove from updating for list
         end
-        -- creative menu, or regular!!!
-        if in_creative then
-            sfinv.set_page(plr, sfinv.get_homepage_name(plr))
-        else
-            sfinv.set_player_inventory_formspec(plr)
-        end
+        -- creative menu, or regular?
+        -- clear player's current context
+        sfinv.set_context(plr)
+        -- set new context based on sfinv.get_homepage_name(plr)
+        sfinv.set_player_inventory_formspec(plr)
         -- powerful hands handling
         -- let us get what we most desire
         if in_creative then

mods/player_api/init.lua
@@ -113,5 +113,6 @@ minetest.register_on_joinplayer(function(player)
     set our page to clothing formspec while player is initialized
     (the formspec needs the texture to display the model)
         ]]
+    sfinv.set_homepage_name(player, "clothing:clothing")
     sfinv.set_page(player, "clothing:clothing")
 end)

commit 9763692a717a6da56980051c455bdd08bd98e291

sfinv: fix crash on 2nd login on a running server (Fixes #1470)
bug introduced by 8cf5c0afcc

mods/sfinv/api.lua
@@ -95,6 +95,9 @@ end
 --          register_on_joinplayer() then you could set the page safely in the
 --          same function or any time later. Otherwise you might see crashes
 --          due to sfinv setting the page too early.
+--          For the same reason never set such a page as the global homepage
+--          for all players.
+--          Otherwise you will see crashes on login or re-login of players.
 function sfinv.set_homepage_name(new_name, player)
     if player then
         local name = player:get_player_name()
@@ -409,14 +412,23 @@ function sfinv.get_page(player)
 	return context and context.page or sfinv.get_homepage_name(player)
 end
 
+-- Sets a default page for each player. (for demo, debugging, fallback)
+-- Mods preferring a different one should set it in another on_join_player()
+-- after all prerequisites for their page are being set up.
 minetest.register_on_joinplayer(function(player)
-	if sfinv.enabled then
-		sfinv.set_player_inventory_formspec(player)
-	end
+    if sfinv.enabled then
+        -- always start with a safe homepage (not one of homepage_overrides)
+        local context = {page = homepage_name}
+        sfinv.set_player_inventory_formspec(player, context)
+    end
 end)
 
 minetest.register_on_leaveplayer(function(player)
-	sfinv.contexts[player:get_player_name()] = nil
+    -- prevent custom pages to be set before per player initialization is done
+    -- otherwise some data required for the page might be nil
+    local player_name = player:get_player_name()
+    sfinv.contexts[player_name] = nil
+    homepage_names[player_name] = nil
 end)
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)