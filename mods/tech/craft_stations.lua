abc2e2b6420b4dd9b385c1bd59efeca5330bc804
Dokimi <dokimi@noreply.codeberg.org>
2026-03-06 11:19:07 +1300

    move the iron using benches to anvil

--- mods/tech/craft_stations.lua
+++ mods/tech/craft_stations.lua
@@ -861,7 +861,7 @@ minetest.register_node(
 })
 
 crafting.register_recipe({ -- from logs for bench and iron for tools
-        type   = {"axe", "carpentry_bench"},
+        type   = {"anvil"},
         output = "tech:carpentry_bench",
         items  = {'tech:iron_ingot 4', 'group:hard_wood 2'},
         level  = 1,
@@ -911,7 +911,7 @@ minetest.register_node(
 })
 
 crafting.register_recipe({ -- from logs for bench and iron for tools
-        type   = {"axe", "carpentry_bench"},
+        type   = {"anvil"},
         output = "tech:masonry_bench",
         items  = {'tech:iron_ingot 4', 'group:hard_wood 2'},
         level  = 1,
